44754345894c497d20838af60b50207ea15d8a2e
cdkersey <chad.d.kersey@gmail.com>
2013-10-07 18:08:28 -0400

    Removed unnecessary return address save.

--- src/test/dotprod.s
+++ src/test/dotprod.s
@@ -25,7 +25,6 @@ entry: ldi %r0, array_a;
 /* Return in r0 dot product of vectors of real values pointed to by r0 and r1,
    length in r2 */
 dotprod: ldi %r3, #0;
-         st %r5, %r3, retaddr;
 dploop:  ld %r4, %r0, #0;
          ld %r6, %r1, #0;
          subi %r2, %r2, #1;
@@ -36,14 +35,11 @@ dploop:  ld %r4, %r0, #0;
          fadd %r3, %r3, %r4;
    @p0 ? jmpi dploop;
          ori %r0, %r3, #0;
-         ldi %r3, retaddr;
-         ld %r5, %r3, #0;
          jmpr %r5;
 
 .align 4096
 .perm rw
 array_a: .word 1.0 2.0 3.0 0.5 1.0 1.5 0.33 0.67 1.0 1.33
 array_b: .word 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1f
-retaddr: .word 0
 
 
