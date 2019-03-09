56706e1f3837ce9e3e6a97c5062eed8c3ba11513
Demo <demo@MBP.local>
2019-03-09 13:57:06 -0500

    finally added a makefile

--- src/riscv_gpgpu/lib/hi.sh
+++ src/riscv_gpgpu/lib/hi.sh
@@ -1,3 +1,3 @@
- /opt/riscv-nommu/bin/riscv32-unknown-linux-gnu-gcc -march=rv32i -mabi=ilp32 -O0 -Wl,-Bstatic,-T,linker.ld -ffreestanding -nostdlib queue.c -o queue.elf
- /opt/riscv-nommu/bin/riscv32-unknown-linux-gnu-objdump -D queue.elf > queue.dump
- /opt/riscv-nommu/bin/riscv32-unknown-linux-gnu-objcopy -O ihex queue.elf queue.hex
+/opt/riscv/bin/riscv32-unknown-elf-gcc -march=rv32i -mabi=ilp32 -O0 -Wl,-Bstatic,-T,linker.ld -ffreestanding -nostdlib queue.c -o queue.elf
+/opt/riscv/bin/riscv32-unknown-elf-objdump -D queue.elf > queue.dump
+/opt/riscv/bin/riscv32-unknown-elf-objcopy -O ihex queue.elf queue.hex
