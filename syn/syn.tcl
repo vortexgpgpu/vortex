0bea82a2c3e016b54dd37bc69283ad1e8d268c85
Shim <dshim8@gtcad-srv3.ece.gatech.edu>
2019-10-17 11:55:18 -0400

    added tcl file

---
+++ syn/syn.tcl
@@ -0,0 +1,40 @@
+set search_path			[concat /nethome/dshim8/Desktop/GTCAD-3DPKG-v3/example/tech/cln28hpm/2d_db/ /nethome/dshim8/Desktop/GTCAD-3DPKG-v3/example/tech/cln28hpm/2d_hard_db/ ../rtl/ ../rtl/interfaces ../rtl/pipe_regs]
+set link_library		[concat * sc12mc_cln28hpm_base_ulvt_c35_ssg_typical_max_0p81v_m40c.db rf2_32_128_wm1_ss_0p81v_0p81v_125c.db dw_foundation.sldb]
+set symbol_library		{}
+set target_library		[concat sc12mc_cln28hpm_base_ulvt_c35_ssg_typical_max_0p81v_m40c.db]
+
+set verilog_files 	[ list  VX_alu.v VX_back_end.v VX_gpr_stage.v VX_gpr_data_inter.v VX_csr_handler.v VX_decode.v VX_define.v VX_execute.v VX_scheduler.v VX_fetch.v VX_forwarding.v VX_front_end.v VX_generic_register.v VX_gpr.v VX_gpr_wrapper.v VX_memory.v VX_one_counter.v VX_priority_encoder.v VX_warp.v VX_warp_scheduler.v VX_writeback.v Vortex.v byte_enabled_simple_dual_port_ram.v VX_branch_response_inter.v VX_csr_write_request_inter.v VX_dcache_request_inter.v VX_dcache_response_inter.v VX_forward_csr_response_inter.v VX_forward_exe_inter.v VX_forward_mem_inter.v VX_forward_reqeust_inter.v VX_forward_response_inter.v VX_forward_wb_inter.v VX_frE_to_bckE_req_inter.v VX_gpr_clone_inter.v VX_gpr_jal_inter.v VX_gpr_read_inter.v VX_gpr_wspawn_inter.v VX_icache_request_inter.v VX_icache_response_inter.v VX_inst_mem_wb_inter.v VX_inst_meta_inter.v VX_jal_response_inter.v VX_mem_req_inter.v VX_mw_wb_inter.v VX_warp_ctl_inter.v VX_wb_inter.v VX_d_e_reg.v VX_e_m_reg.v VX_f_d_reg.v VX_m_w_reg.v \
+					]
+
+analyze -format sverilog $verilog_files
+elaborate Vortex
+link
+
+set clk_freq 100
+set clk_period [expr 1000.0 / $clk_freq / 1.0]
+create_clock [get_ports clk] -period $clk_period
+set_max_fanout 20 [get_ports clk]
+set_ideal_network [get_ports clk]
+
+set_max_fanout 20 [get_ports reset]
+set_false_path -from [get_ports reset]
+
+compile_ultra -no_autoungroup
+ungroup -all -flatten
+uniquify
+
+define_name_rules verilog -remove_internal_net_bus -remove_port_bus
+change_names -rule verilog -hierarchy
+
+report_qor 
+report_area
+report_hierarchy
+report_cell
+report_reference
+report_port
+
+write -hierarchy -format verilog -output Vortex.netlist.v
+remove_ideal_network [get_ports clk]
+set_propagated_clock [get_ports clk]
+write_sdc -version 1.9 Vortex.sdc
+exit
