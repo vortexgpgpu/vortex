c79d08e12cf1f7446961f99543ee35f0eaa31bb2
Lyons, Ethan Tyler <ethan.lyons@gatech.edu>
2019-11-08 10:56:44 -0500

    Add files via upload
    ICache_In_Place

--- rtl/interfaces/VX_icache_response_inter.v
+++ rtl/interfaces/VX_icache_response_inter.v
@@ -1,4 +1,3 @@
-
 `include "../VX_define.v"
 
 `ifndef VX_ICACHE_RSP
@@ -10,6 +9,7 @@ interface VX_icache_response_inter ();
 	// wire ready;
 	// wire stall;
 	wire[31:0] instruction;
+	wire       delay;
 
 
 endinterface
