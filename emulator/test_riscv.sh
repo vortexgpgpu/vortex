13ebe51f0364c8011f59109e2a80588520ea6e85
fares <felsabbagh3@gatech.edu>
2019-11-21 00:24:42 -0500

    Created a global linker file to fix descrepencies

--- emulator/test_riscv.sh
+++ emulator/test_riscv.sh
@@ -1,5 +1,5 @@
 echo start > results.txt
 
 # echo ../kernel/vortex_test.hex
-./harptool -E -a rv32i --core ../runtime/mains/nativevecadd/vx_pocl_main.hex  -s -b 1> emulator.debug
+./harptool -E -a rv32i --core ../runtime/mains/simple/vx_simple_main.hex  -s -b 1> emulator.debug
 # ./harptool -E -a rv32i --core ../runtime/mains/vector_test/vx_vector_main.hex  -s -b 1> emulator.debug
