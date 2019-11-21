7f3282d5a55e80413c421d01ff2863d81e3ded62
proshan3 <priya@DESKTOP-B2EEP7S.localdomain>
2019-11-21 17:07:49 -0500

    Vector changes

--- runtime/mains/vector_test/vx_vec.s
+++ runtime/mains/vector_test/vx_vec.s
@@ -4,4 +4,12 @@
 .type vx_vec_test, @function
 .global vx_vec_test
 vx_vec_test:
+	li a0, 2
+	vsetvli t0, a0, e32, m1
+	li  a0, 10
+	sw  a0, 0(a1)
+	sw  a0, 32(a1)
+	vlw.v   v0, (a1)
+	vadd.vv v0, v0, v0
+	vsw.v  v0, (a1)
 	ret
