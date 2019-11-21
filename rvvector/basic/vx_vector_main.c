7a9a1efc46065aef63e0df3055dc186af502ee2c
fares <felsabbagh3@gatech.edu>
2019-11-21 11:22:20 -0500

    basic rvvector example

--- rvvector/basic/vx_vector_main.c
+++ rvvector/basic/vx_vector_main.c
@@ -5,6 +5,28 @@
 int main()
 {
 	vx_tmc(1);
-	vx_vec_test();
+	// int * a = malloc(4);
+	// int * b = malloc(4);
+	// int * c = malloc(4);
+
+
+	int * a = malloc(4);
+	*a = 5;
+	printf("Value of a: %d\n", *a);
+
+	vx_vec_test(a);
+
+	printf("Value of a: %d\n", *a);
+
+
+	// for (int i = 0; i < 4; i++)
+	// {
+	// 	if (c[i] != (a[i] + b[i]))
+	// 	{
+	// 		printf("Fail\n");
+	// 		break;
+	// 	}
+	// }
+
 	vx_tmc(0);
 }
