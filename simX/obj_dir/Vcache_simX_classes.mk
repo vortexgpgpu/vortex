6e0b59caa9f10673ebbac8b21a1168291a78cb12
proshan3 <priya@DESKTOP-B2EEP7S.localdomain>
2019-11-22 08:06:25 -0500

    Vector mask changes

---
+++ simX/obj_dir/Vcache_simX_classes.mk
@@ -0,0 +1,47 @@
+# Verilated -*- Makefile -*-
+# DESCRIPTION: Verilator output: Make include file with class lists
+#
+# This file lists generated Verilated files, for including in higher level makefiles.
+# See Vcache_simX.mk for the caller.
+
+### Switches...
+# Coverage output mode?  0/1 (from --coverage)
+VM_COVERAGE = 0
+# Threaded output mode?  0/1/N threads (from --threads)
+VM_THREADS = 0
+# Tracing output mode?  0/1 (from --trace)
+VM_TRACE = 1
+# Tracing threadeds output mode?  0/1 (from --trace-fst-thread)
+VM_TRACE_THREADED = 0
+
+### Object file lists...
+# Generated module classes, fast-path, compile with highest optimization
+VM_CLASSES_FAST += \
+	Vcache_simX \
+	Vcache_simX_VX_dram_req_rsp_inter__N4_NB4 \
+	Vcache_simX_VX_dram_req_rsp_inter__N1_NB4 \
+	Vcache_simX_VX_dcache_request_inter \
+	Vcache_simX_VX_Cache_Bank__pi7 \
+
+# Generated module classes, non-fast-path, compile with low/medium optimization
+VM_CLASSES_SLOW += \
+
+# Generated support classes, fast-path, compile with highest optimization
+VM_SUPPORT_FAST += \
+	Vcache_simX__Trace \
+
+# Generated support classes, non-fast-path, compile with low/medium optimization
+VM_SUPPORT_SLOW += \
+	Vcache_simX__Syms \
+	Vcache_simX__Trace__Slow \
+
+# Global classes, need linked once per executable, fast-path, compile with highest optimization
+VM_GLOBAL_FAST += \
+	verilated \
+	verilated_vcd_c \
+
+# Global classes, need linked once per executable, non-fast-path, compile with low/medium optimization
+VM_GLOBAL_SLOW += \
+
+
+# Verilated -*- Makefile -*-
---
+++ simX/obj_dir/instruction.d
@@ -0,0 +1,10 @@
+instruction.o: ../instruction.cpp ../include/instruction.h \
+ ../include/types.h ../include/trace.h ../include/obj.h \
+ ../include/archdef.h ../include/instruction.h ../include/enc.h \
+ ../include/obj.h ../include/asm-tokens.h ../include/core.h \
+ ../include/mem.h ../include/debug.h Vcache_simX.h \
+ /usr/local/share/verilator/include/verilated.h \
+ /usr/local/share/verilator/include/verilatedos.h \
+ /usr/local/share/verilator/include/verilated_vcd_c.h \
+ /usr/local/share/verilator/include/verilated.h ../include/harpfloat.h \
+ ../include/debug.h
