9e58bf8fb584a97dbf5d029708b2a92df886eba7
fares <felsabbagh3@gatech.edu>
2019-11-22 00:32:19 -0500

    Started synthesis script

--- syn/run_mult_synth.sh
+++ syn/run_mult_synth.sh
@@ -11,7 +11,7 @@ for number_of_warps in 2 4 8 16 32; do
             echo "Warp Count: $number_of_warps  Thread Count: $number_of_threads Launched"
             echo "\`define NT $number_of_threads" > ../rtl/VX_define_synth.v
             echo "\`define NW $number_of_warps" >> ../rtl/VX_define_synth.v
-            make dc | tee run.log
+            make dc | tee run.log 1>/dev/null
             sleep 30
             moved_filename="${number_of_warps}_Warps__${number_of_threads}_threads__400MHz.log"
             mv ./vortex_syn.log ../../$moved_filename
