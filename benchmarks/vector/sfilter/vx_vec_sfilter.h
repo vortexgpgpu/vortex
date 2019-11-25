2fb1a0a1da48ed28e9a153b2d7ea8140c1c42985
proshan3 <priya@DESKTOP-B2EEP7S.localdomain>
2019-11-25 11:53:02 -0500

    sfilter working

---
+++ benchmarks/vector/sfilter/vx_vec_sfilter.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void vx_vec_sfilter(int* a, int* b, int ldc, int m, int x, int y, int N);
+
+#ifdef __cplusplus
+}
+#endif                   
