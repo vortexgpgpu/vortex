36547821fcf1610f6006ea5219588075000e3d29
Blaise Tine <tinebp@yahoo.com>
2020-03-09 10:18:11 -0400

    temp files cleanup

--- simX/obj_dir/verilated_vcd_c.d
+++
@@ -1,5 +0,0 @@
-verilated_vcd_c.o: /usr/share/verilator/include/verilated_vcd_c.cpp \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_config.h \
- /usr/share/verilator/include/verilated_vcd_c.h
