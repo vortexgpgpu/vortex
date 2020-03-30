b15c9aa8117b95f691c176d504ae18839d710734
felsabbagh3 <felsabbagh3@gatech.edu>
2020-03-29 17:28:57 -0700

    ..

--- rtl/VX_cache/VX_tag_data_access.v
+++ rtl/VX_cache/VX_tag_data_access.v
@@ -278,9 +278,9 @@ module VX_tag_data_access
 	wire tags_mismatch = writeaddr_tag != use_read_tag_st1e;
 	wire tags_match    = writeaddr_tag == use_read_tag_st1e;
 
-	wire snoop_hit    = valid_req_st1e &&  is_snp_st1e && tags_mismatch;
+	wire snoop_hit    = valid_req_st1e &&  is_snp_st1e && tags_match;
 	wire req_invalid  = valid_req_st1e && !is_snp_st1e && !use_read_valid_st1e && !writefill_st1e;
-	wire req_miss     = valid_req_st1e && !is_snp_st1e &&  use_read_valid_st1e && !writefill_st1e && tags_match;
+	wire req_miss     = valid_req_st1e && !is_snp_st1e &&  use_read_valid_st1e && !writefill_st1e && tags_mismatch;
 	
 	assign miss_st1e           = snoop_hit || req_invalid || req_miss;
 	assign dirty_st1e          = valid_req_st1e && use_read_valid_st1e && use_read_dirty_st1e;
