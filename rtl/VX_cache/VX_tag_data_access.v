f96d77d75e0ab7d80bc75a9d3e4d7c123ddd8d34
felsabbagh3 <felsabbagh3@gatech.edu>
2020-03-29 17:18:57 -0700

    Mismatched vs matched

--- rtl/VX_cache/VX_tag_data_access.v
+++ rtl/VX_cache/VX_tag_data_access.v
@@ -275,9 +275,10 @@ module VX_tag_data_access
 
 	wire[`TAG_SELECT_ADDR_RNG] writeaddr_tag = writeaddr_st1e[`TAG_SELECT_ADDR_RNG];
 
-	wire tags_match   = writeaddr_tag != use_read_tag_st1e;
-	
-	wire snoop_hit    = valid_req_st1e &&  is_snp_st1e && tags_match;
+	wire tags_mismatch = writeaddr_tag != use_read_tag_st1e;
+	wire tags_match    = writeaddr_tag == use_read_tag_st1e;
+
+	wire snoop_hit    = valid_req_st1e &&  is_snp_st1e && tags_mismatch;
 	wire req_invalid  = valid_req_st1e && !is_snp_st1e && !use_read_valid_st1e && !writefill_st1e;
 	wire req_miss     = valid_req_st1e && !is_snp_st1e &&  use_read_valid_st1e && !writefill_st1e && tags_match;
 	
