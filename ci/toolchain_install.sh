b82f5a90117310f64f862fd720edc0315482abb6
Blaise Tine <tinebp@iam-ssh1.research.intel-research.net>
2020-09-01 10:45:44 -0700

    fix ci bui;d

---
+++ ci/toolchain_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+REPOSITORY=https://github.com/vortexgpgpu/vortex-toolchain-prebuilt/raw/master
+
+# RISCV-GNU_TOOLCHAIN
+
+for x in {a..o} 
+do
+    wget $REPOSITORY/riscv-gnu-toolchain/ubuntu/bionic/riscv-gnu-toolchain.tar.bz2.parta$x
+done
+
+cat riscv-gnu-toolchain.tar.bz2.parta* > riscv-gnu-toolchain.tar.bz2
+tar -xvf riscv-gnu-toolchain.tar.bz2
+rm -f riscv-gnu-toolchain.tar.bz2*
+
+# VERILATOR
+
+wget $REPOSITORY/verilator/ubuntu/bionic/verilator.bz2
+tar -xvf verilator.bz2
+rm -f verilator.bz2
