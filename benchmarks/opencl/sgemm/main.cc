37a19873d70119e0ea1696b1ef91e39fbcba2585
Blaise Tine <tinebp@yahoo.com>
2020-09-04 10:45:25 -0400

    fixed POCL device cleanup

--- benchmarks/opencl/sgemm/main.cc
+++ benchmarks/opencl/sgemm/main.cc
@@ -34,25 +34,27 @@
 
 #define NUM_DATA 64
 
-#define CL_CHECK(_expr)                                                        \
-  do {                                                                         \
-    cl_int _err = _expr;                                                       \
-    if (_err == CL_SUCCESS)                                                    \
-      break;                                                                   \
-    fprintf(stderr, "OpenCL Error: '%s' returned %d!\n", #_expr, (int)_err);   \
-    abort();                                                                   \
-  } while (0)
+#define CL_CHECK(_expr)                                                \
+   do {                                                                \
+     cl_int _err = _expr;                                              \
+     if (_err == CL_SUCCESS)                                           \
+       break;                                                          \
+     printf("OpenCL Error: '%s' returned %d!\n", #_expr, (int)_err);   \
+	 cleanup();			                                                     \
+     exit(-1);                                                         \
+   } while (0)
 
-#define CL_CHECK_ERR(_expr)                                                    \
-  ({                                                                           \
-    cl_int _err = CL_INVALID_VALUE;                                            \
-    decltype(_expr) _ret = _expr;                                              \
-    if (_err != CL_SUCCESS) {                                                  \
-      fprintf(stderr, "OpenCL Error: '%s' returned %d!\n", #_expr, (int)_err); \
-      abort();                                                                 \
-    }                                                                          \
-    _ret;                                                                      \
-  })
+#define CL_CHECK2(_expr)                                               \
+   ({                                                                  \
+     cl_int _err = CL_INVALID_VALUE;                                   \
+     decltype(_expr) _ret = _expr;                                     \
+     if (_err != CL_SUCCESS) {                                         \
+       printf("OpenCL Error: '%s' returned %d!\n", #_expr, (int)_err); \
+	   cleanup();			                                                   \
+       exit(-1);                                                       \
+     }                                                                 \
+     _ret;                                                             \
+   })
 
 void pfn_notify(const char *errinfo, const void *private_info, size_t cb,
                 void *user_data) {
@@ -80,37 +82,34 @@ static int read_kernel_file(const char* filename, uint8_t** data, size_t* size) 
   return 0;
 }
 
+cl_device_id device_id = NULL;
 uint8_t *kernel_bin = NULL;
+cl_context context = 0;
+cl_kernel kernel = 0;
+cl_command_queue queue = 0;
+cl_program program = 0;
+cl_mem memObjects[3] = {0, 0, 0};
 
 ///
 //  Cleanup any created OpenCL resources
 //
-void Cleanup(cl_context context, cl_command_queue commandQueue,
-             cl_program program, cl_kernel kernel, cl_mem memObjects[3]) {
+void cleanup() {
   for (int i = 0; i < 3; i++) {
-    if (memObjects[i] != 0)
-      clReleaseMemObject(memObjects[i]);
+    if (memObjects[i]) clReleaseMemObject(memObjects[i]);
   }
-  if (commandQueue != 0)
-    clReleaseCommandQueue(commandQueue);
-
-  if (kernel != 0)
-    clReleaseKernel(kernel);
-
-  if (program != 0)
-    clReleaseProgram(program);
-
-  if (context != 0)
-    clReleaseContext(context);
-
+  if (queue) clReleaseCommandQueue(queue);
+  if (kernel) clReleaseKernel(kernel);
+  if (program) clReleaseProgram(program);
+  if (context) clReleaseContext(context);
+  if (device_id) clReleaseDevice(device_id); 
+  
   if (kernel_bin) free(kernel_bin);
 }
 
 int main(int argc, char **argv) {
   printf("enter demo main\n");
   
-  cl_platform_id platform_id;
-  cl_device_id device_id;
+  cl_platform_id platform_id;  
   size_t kernel_size;
   cl_int binary_status = 0;
   int i;
@@ -122,17 +121,11 @@ int main(int argc, char **argv) {
   // Getting platform and device information
   CL_CHECK(clGetPlatformIDs(1, &platform_id, NULL));
   CL_CHECK(clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, NULL));
-
-  cl_context context;
-  context = CL_CHECK_ERR(
-      clCreateContext(NULL, 1, &device_id, &pfn_notify, NULL, &_err));
-
-  cl_command_queue queue;
-  queue = CL_CHECK_ERR(clCreateCommandQueue(context, device_id,
-                                            CL_QUEUE_PROFILING_ENABLE, &_err));
-
-  cl_kernel kernel = 0;
-  cl_mem memObjects[3] = {0, 0, 0};
+  
+  context = CL_CHECK2(clCreateContext(NULL, 1, &device_id, &pfn_notify, NULL, &_err));
+  
+  queue = CL_CHECK2(clCreateCommandQueue(context, device_id,
+                                            CL_QUEUE_PROFILING_ENABLE, &_err));  
 
   // Create OpenCL program - first attempt to load cached binary.
   //  If that is not available, then create the program from source
@@ -140,15 +133,13 @@ int main(int argc, char **argv) {
   std::cout << "Attempting to create program from binary..." << std::endl;
   // cl_program program = CreateProgramFromBinary(context, device_id,
   // "kernel.cl.bin");
-  cl_program program = CL_CHECK_ERR(clCreateProgramWithBinary(
+  program = CL_CHECK2(clCreateProgramWithBinary(
     context, 1, &device_id, &kernel_size, &kernel_bin, &binary_status, &_err));
   if (program == NULL) {
-    std::cerr << "Failed to write program binary" << std::endl;
-    Cleanup(context, queue, program, kernel, memObjects);
-    return 1;
-  } else {
-    std::cout << "Read program from binary." << std::endl;
-  }
+    printf("clCreateProgramWithBinary() failed\n");
+    cleanup();
+    return -1;
+  } 
 
   // Build program
   CL_CHECK(clBuildProgram(program, 1, &device_id, NULL, NULL, NULL));
@@ -156,19 +147,19 @@ int main(int argc, char **argv) {
   printf("attempting to create input buffer\n");
   fflush(stdout);
   cl_mem input_bufferA;
-  input_bufferA = CL_CHECK_ERR(
+  input_bufferA = CL_CHECK2(
       clCreateBuffer(context, CL_MEM_READ_ONLY,
                      sizeof(float) * NUM_DATA * NUM_DATA, NULL, &_err));
 
   cl_mem input_bufferB;
-  input_bufferB = CL_CHECK_ERR(
+  input_bufferB = CL_CHECK2(
       clCreateBuffer(context, CL_MEM_READ_ONLY,
                      sizeof(float) * NUM_DATA * NUM_DATA, NULL, &_err));
 
   printf("attempting to create output buffer\n");
   fflush(stdout);
   cl_mem output_buffer;
-  output_buffer = CL_CHECK_ERR(
+  output_buffer = CL_CHECK2(
       clCreateBuffer(context, CL_MEM_WRITE_ONLY,
                      sizeof(float) * NUM_DATA * NUM_DATA, NULL, &_err));
 
@@ -180,7 +171,7 @@ int main(int argc, char **argv) {
 
   printf("attempting to create kernel\n");
   fflush(stdout);
-  kernel = CL_CHECK_ERR(clCreateKernel(program, "sgemm", &_err));
+  kernel = CL_CHECK2(clCreateKernel(program, "sgemm", &_err));
   CL_CHECK(clSetKernelArg(kernel, 0, sizeof(input_bufferA), &input_bufferA));
   CL_CHECK(clSetKernelArg(kernel, 1, sizeof(input_bufferB), &input_bufferB));
   CL_CHECK(clSetKernelArg(kernel, 2, sizeof(output_buffer), &output_buffer));
@@ -209,7 +200,7 @@ int main(int argc, char **argv) {
   CL_CHECK(clEnqueueNDRangeKernel(queue, kernel, 3, NULL, global_work_size,
                                   local_work_size, 0, NULL,
                                   &kernel_completion));
-  printf("Enqueue'd kerenel\n");
+  printf("Enqueue'd kernel\n");
   fflush(stdout);
   cl_ulong time_start, time_end;
   CL_CHECK(clWaitForEvents(1, &kernel_completion));
@@ -231,13 +222,8 @@ int main(int argc, char **argv) {
   }
   printf("\n");
 
-  CL_CHECK(clReleaseMemObject(memObjects[0]));
-  CL_CHECK(clReleaseMemObject(memObjects[1]));
-  CL_CHECK(clReleaseMemObject(memObjects[2]));
-
-  CL_CHECK(clReleaseKernel(kernel));
-  CL_CHECK(clReleaseProgram(program));
-  CL_CHECK(clReleaseContext(context));
+  // Clean up		
+  cleanup();  
 
   return 0;
 }
