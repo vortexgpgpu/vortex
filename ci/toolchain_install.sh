25c40e864d113e3ff386813b62d194b8563fb4cb
Blaise Tine <tinebp@yahoo.com>
2020-09-19 17:00:45 -0400

    update CI scripts

--- ci/toolchain_install.sh
+++ ci/toolchain_install.sh
@@ -2,42 +2,74 @@
 
 REPOSITORY=https://github.com/vortexgpgpu/vortex-toolchain-prebuilt/raw/master
 
-# RISCV-GNU_TOOLCHAIN
+riscv()
+{
+    for x in {a..o} 
+    do
+        wget $REPOSITORY/riscv-gnu-toolchain/ubuntu/bionic/riscv-gnu-toolchain.tar.bz2.parta$x
+    done
+    cat riscv-gnu-toolchain.tar.bz2.parta* > riscv-gnu-toolchain.tar.bz2
+    tar -xvf riscv-gnu-toolchain.tar.bz2
+    rm -f riscv-gnu-toolchain.tar.bz2*
+    sudo cp riscv-gnu-toolchain /opt/
+    rm -rf riscv-gnu-toolchain
+}
 
-for x in {a..o} 
-do
-    wget $REPOSITORY/riscv-gnu-toolchain/ubuntu/bionic/riscv-gnu-toolchain.tar.bz2.parta$x
+llvm()
+{
+    for x in {a..f} 
+    do
+        wget $REPOSITORY/llvm-riscv/ubuntu/bionic/llvm-riscv.tar.bz2.parta$x
+    done
+    tar -xvf llvm-riscv.tar.bz2
+    rm -f llvm-riscv.tar.bz2
+    sudo cp llvm-riscv /opt/
+    rm -rf llvm-riscv
+}
+
+pocl()
+{
+    wget $REPOSITORY/pocl/ubuntu/bionic/pocl.tar.bz2
+    tar -xvf pocl.bz2
+    rm -f pocl.bz2
+    sudo cp pocl /opt/
+    rm -rf pocl
+}
+
+verilator()
+{
+    wget $REPOSITORY/verilator/ubuntu/bionic/verilator.tar.bz2
+    tar -xvf verilator.tar.bz2
+    rm -f verilator.tar.bz2
+    sudo cp verilator /opt/
+    rm -rf verilator
+}
+
+usage()
+{
+    echo "usage: toolchain_install [[-riscv] [-llvm] [-pocl] [-verilator] [-all] [-h|--help]]"
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -pocl ) pocl
+                ;;
+        -verilator ) verilator
+                     ;;
+        -riscv ) riscv
+                 ;;
+        -llvm ) llvm
+                ;;
+        -all ) riscv
+               llvm
+               pocl
+               verilator
+               ;;
+        -h | --help ) usage
+                      exit
+                      ;;
+        * )           usage
+                      exit 1
+    esac
+    shift
 done
-
-cat riscv-gnu-toolchain.tar.bz2.parta* > riscv-gnu-toolchain.tar.bz2
-tar -xvf riscv-gnu-toolchain.tar.bz2
-rm -f riscv-gnu-toolchain.tar.bz2*
-sudo cp opt/riscv-gnu-toolchain /opt/
-rm -rf riscv-gnu-toolchain
-
-# LLVM
-
-for x in {a..f} 
-do
-    wget $REPOSITORY/llvm-riscv/ubuntu/bionic/llvm-riscv.tar.bz2.parta$x
-done
-tar -xvf llvm-riscv.tar.bz2
-rm -f llvm-riscv.tar.bz2
-sudo cp opt/llvm-riscv /opt/
-rm -rf llvm-riscv
-
-# POCL
-
-wget $REPOSITORY/pocl/ubuntu/bionic/pocl.tar.bz2
-tar -xvf pocl.bz2
-rm -f pocl.bz2
-sudo cp opt/pocl /opt/
-rm -rf pocl
-
-# VERILATOR
-
-wget $REPOSITORY/verilator/ubuntu/bionic/verilator.tar.bz2
-tar -xvf verilator.tar.bz2
-rm -f verilator.tar.bz2
-sudo cp opt/verilator /opt/
-rm -rf verilator
