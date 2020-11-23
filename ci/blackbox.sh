e281d3213880158636208cea4b7f09037506750d
Blaise Tine <tinebp@iam-ssh1.research.intel-research.net>
2020-11-22 19:07:46 -0800

    travis timeout workaround

--- ci/blackbox.sh
+++ ci/blackbox.sh
@@ -6,7 +6,7 @@ set -e
 show_usage()
 {
     echo "Vortex BlackBox Test Driver v1.0"
-    echo "Usage: [[--clusters=#n] [--cores=#n] [--warps=#n] [--threads=#n] [--l2cache] [[--driver=rtlsim|vlsim] [--debug] [--scope] [--travis] [--app=vecadd|sgemm|basic|demo|dogfood] [--args=<args>] [--help]]"
+    echo "Usage: [[--clusters=#n] [--cores=#n] [--warps=#n] [--threads=#n] [--l2cache] [[--driver=rtlsim|vlsim] [--debug] [--scope] [--app=vecadd|sgemm|basic|demo|dogfood] [--args=<args>] [--help]]"
 }
 
 DRIVER=vlsim
@@ -18,7 +18,6 @@ THREADS=4
 L2=0
 DEBUG=0
 SCOPE=0
-TRAVIS=0
 HAS_ARGS=0
 
 for i in "$@"
@@ -65,17 +64,13 @@ case $i in
         HAS_ARGS=1
         shift
         ;;
-    --travis)
-        TRAVIS=1
-        shift
-        ;;
     --help)
         show_usage
-        exit
+        exit 0
         ;;
     *)
     show_usage   
-    exit       
+    exit -1       
     ;;
 esac
 done
@@ -91,7 +86,7 @@ case $DRIVER in
         ;;
     *)
         echo "invalid driver: $DRIVER"
-        exit
+        exit -1
         ;;
 esac
 
@@ -113,7 +108,7 @@ case $APP in
         ;;
     *)
         echo "invalid app: $APP"
-        exit
+        exit -1
         ;;
 esac
 
@@ -124,13 +119,7 @@ echo "CONFIGS=$CONFIGS"
 make -C $DRIVER_PATH clean
 
 if [ $DEBUG -eq 1 ]
-then
-    if [ $TRAVIS -eq 1 ]
-    then
-        while sleep 5m; do echo "*** still running..."; done &
-        sleep_pid=$!
-    fi
-    
+then    
     if [ $SCOPE -eq 1 ]
     then
         DEBUG=1 SCOPE=1 CONFIGS="$CONFIGS" make -C $DRIVER_PATH $DRIVER_EXTRA > build.log 2>&1
@@ -144,28 +133,12 @@ then
     else
         make -C $APP_PATH run-$DRIVER > run.log 2>&1
     fi
-
-    if [ $TRAVIS -eq 1 ]
-    then
-        kill $sleep_pid
-    fi
 else
-    if [ $TRAVIS -eq 1 ]
-    then
-        while sleep 5m; do echo "*** still running..."; done &
-        sleep_pid=$!
-    fi
-
     if [ $SCOPE -eq 1 ]
     then
         SCOPE=1 CONFIGS="$CONFIGS" make -C $DRIVER_PATH $DRIVER_EXTRA > build.log 2>&1
     else
         CONFIGS="$CONFIGS" make -C $DRIVER_PATH $DRIVER_EXTRA > build.log 2>&1
-    fi
-
-    if [ $TRAVIS -eq 1 ]
-    then
-        kill $sleep_pid
     fi
     
     if [ $HAS_ARGS -eq 1 ]
