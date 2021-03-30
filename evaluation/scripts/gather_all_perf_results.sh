c89f735185bdf25d451e52e2bc7aef1ba594211e
Liam Paul Cooper <lcooper43@flubber2.crnch.gatech.edu>
2021-03-30 16:32:35 -0400

    Fixed script creating dir in wrong location

--- evaluation/scripts/gather_all_perf_results.sh
+++ evaluation/scripts/gather_all_perf_results.sh
@@ -19,13 +19,17 @@ for ((i=1; i <= 16; i=i*2)); do
 done
 
 cd ../../../evaluation/scripts
+results_dir="../perf_${date}"
 
 for ((i=1; i <= 16; i=i*2)); do
+  echo "Programming fpga for ${i} core build..."
   ./program_fpga.sh -c ${i}
+  echo "Running tests for ${i} core build..."
   ../../ci/blackbox.sh --driver=fpga --app=sgemm --perf > "${results_dir}/${i}c/sgemm.result"
   ../../ci/blackbox.sh --driver=fpga --app=vecadd --perf > "${results_dir}/${i}c/vecadd.result"
   ../../ci/blackbox.sh --driver=fpga --app=saxpy --perf > "${results_dir}/${i}c/saxpy.result"
   ../../ci/blackbox.sh --driver=fpga --app=sfilter --perf > "${results_dir}/${i}c/sfilter.result"
   ../../ci/blackbox.sh --driver=fpga --app=nearn --perf > "${results_dir}/${i}c/nearn.result"
   ../../ci/blackbox.sh --driver=fpga --app=guassian --perf > "${results_dir}/${i}c/guassian.result"
+  echo "Done ${i} core build."
 done
