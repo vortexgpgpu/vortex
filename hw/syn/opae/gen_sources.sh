eda784da40b78a736241fde78535a511eba45d6c
Blaise Tine <tinebp@yahoo.com>
2021-04-04 18:25:26 -0700

    minor update

--- hw/syn/opae/gen_sources.sh
+++ hw/syn/opae/gen_sources.sh
@@ -31,7 +31,7 @@ add_files()
 
 add_dirs $rtl_dir/fp_cores/altera/$1
 
-add_dirs $rtl_dir/libs $rtl_dir/interfaces $rtl_dir/fp_cores $rtl_dir/cache $rtl_dir $rtl_dir/afu
+add_dirs $rtl_dir/libs $rtl_dir/interfaces $rtl_dir/fp_cores $rtl_dir/cache $rtl_dir/tex_unit $rtl_dir $rtl_dir/afu
 
 # dump file list
 for file in $file_list; do
