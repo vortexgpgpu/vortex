67f3ca4b17c17c80729910bfd3f1a382f5e6c02a
Blaise Tine <tinebp@yahoo.com>
2021-04-15 22:34:04 -0700

    text sw emulation

--- driver/tests/tex_demo/utils.h
+++ driver/tests/tex_demo/utils.h
@@ -3,6 +3,12 @@
 #include <iostream>
 #include "blitter.h"
 
+#define ISPOW2(x)   (((x) != 0) && (0 == ((x) & ((x) - 1))))
+
+inline uint32_t ilog2 (uint32_t value) {
+  	return (uint32_t)(sizeof(uint32_t) * 8UL) - (uint32_t)__builtin_clzl((value << 1) - 1UL) - 1;
+}
+
 int LoadTGA(const char *filename, 
             std::vector<uint8_t> &pixels, 
             uint32_t *width,
