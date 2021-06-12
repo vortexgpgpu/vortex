befda3e187aa36ab01a7c196985762cedef3a608
Blaise Tine <tinebp@hotmail.com>
2021-06-12 02:22:01 -0400

    minor update

--- runtime/src/vx_perf.c
+++ runtime/src/vx_perf.c
@@ -21,7 +21,7 @@
 
 void vx_perf_dump() {
     int core_id = vx_core_id();
-    uint32_t* const csr_mem = (uint32_t*)(IO_ADDR_CSR + 64 * sizeof(uint32_t) * core_id);
+    uint32_t* const csr_mem = (uint32_t*)(IO_CSR_ADDR + 64 * sizeof(uint32_t) * core_id);
     DUMP_CSR_32(0,  CSR_MPM_BASE)
     DUMP_CSR_32(32, CSR_MPM_BASE_H)
 }
