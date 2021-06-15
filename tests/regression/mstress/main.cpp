f7bc14a2ecb3d448dc76f89f50f6e905935f80da
Blaise Tine <tinebp@hotmail.com>
2021-06-15 13:26:14 -0400

    adding printf regression test

--- tests/regression/mstress/main.cpp
+++ tests/regression/mstress/main.cpp
@@ -75,7 +75,7 @@ vx_device_h device = nullptr;
 vx_buffer_h staging_buf = nullptr;
 
 static void show_usage() {
-   std::cout << "Vortex Driver Test." << std::endl;
+   std::cout << "Vortex Test." << std::endl;
    std::cout << "Usage: [-k: kernel] [-n words] [-h: help]" << std::endl;
 }
 
---
+++ tests/regression/printf/kernel.c
@@ -0,0 +1,15 @@
+#include <stdint.h>
+#include <vx_intrinsics.h>
+#include <vx_spawn.h>
+#include "common.h"
+
+void kernel_body(int task_id, void* arg) {
+	struct kernel_arg_t* _arg = (struct kernel_arg_t*)(arg);
+	int* src_ptr = (int*)_arg->src_ptr;
+	vx_printf("task=%d, value=%d\n", task_id, src_ptr[task_id]);
+}
+
+void main() {
+	struct kernel_arg_t* arg = (struct kernel_arg_t*)KERNEL_ARG_DEV_MEM_ADDR;
+	vx_spawn_tasks(arg->num_points, kernel_body, arg);
+}
