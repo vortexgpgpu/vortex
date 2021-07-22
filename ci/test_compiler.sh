1ba2701bc501453bcc3d65c452fa6966e999e638
Blaise Tine <tinebp@yahoo.com>
2021-07-22 14:20:02 -0700

    minor update

--- ci/test_compiler.sh
+++ ci/test_compiler.sh
@@ -10,17 +10,18 @@ rm -rf ~/.cache/pocl
 make -C runtime clean
 make -C runtime
 
-# rebuild runtime test
-make -C tests/runtime/simple clean
-make -C tests/runtime/simple
-make -C tests/runtime/simple run-simx
+# rebuild drivers
+make -C driver clean
+make -C driver
 
-# rebuild native kernel
-make -C tests/regression/dogfood clean-all
-make -C tests/regression/dogfood
-make -C tests/regression/dogfood run-simx
+# rebuild runtime tests
+make -C tests/runtime clean
+make -C tests/runtime
 
-# rebuild opencl kernel
-make -C tests/opencl/sgemm clean-all
-make -C tests/opencl/sgemm
-make -C tests/opencl/sgemm run-simx
+# rebuild regression tests
+make -C tests/regression clean-all
+make -C tests/regression
+
+# rebuild opencl tests
+make -C tests/opencl clean-all
+make -C tests/opencl
