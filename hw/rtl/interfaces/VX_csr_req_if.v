05bc970900c9a56bd1c306deeba7d731d1e09870
Blaise Tine <tinebp@yahoo.com>
2021-09-07 23:57:14 -0700

    minor update

--- hw/rtl/interfaces/VX_csr_req_if.v
+++ hw/rtl/interfaces/VX_csr_req_if.v
@@ -13,7 +13,7 @@ interface VX_csr_req_if ();
     wire [`CSR_ADDR_BITS-1:0] addr;
     wire [31:0]             rs1_data;
     wire                    use_imm;
-    wire [`NR_BITS-1:0]     rs1;
+    wire [`NRI_BITS-1:0]    imm;
     wire [`NR_BITS-1:0]     rd;
     wire                    wb;    
     wire                    ready;
