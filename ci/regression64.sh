8a525039af936747a00fce94238d10946433d7a4
Santosh Srivatsan <santosh.srivatsan@gmail.com>
2022-02-03 15:20:15 -0500

    Added 64 bit regression script

---
+++ ci/regression64.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+# ensure build
+XLEN=64 make -s
+
+coverage() 
+{
+echo "begin coverage tests..."
+
+make -C tests/riscv/isa run-simx-64imfd
+
+echo "coverage tests done!"
+}
+
+usage()
+{
+    echo "usage: regression [-coverage] [-all] [-h|--help]"
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -coverage ) coverage
+                ;;
+        -all ) coverage
+                ;;
+        -h | --help ) usage
+                      exit
+                ;;
+        * )           usage
+                      exit 1
+    esac
+    shift
+done
