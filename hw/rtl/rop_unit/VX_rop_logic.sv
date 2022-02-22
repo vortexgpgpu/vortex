080c345252c943268f2cfd4e0351eac57f680628
Joshua Simpson <jsimps03@calpoly.edu>
2022-02-22 10:50:38 -0800

    Fixed Logic Op to match meeting notes

--- hw/rtl/rop_unit/VX_rop_logic.sv
+++ hw/rtl/rop_unit/VX_rop_logic.sv
@@ -3,59 +3,35 @@
 module VX_rop_logic #(  
     parameter CORE_ID = 0
 ) (
-    input wire clk,
-    input wire reset,
-
     // Inputs
-    input rop_csrs_t rop_csrs,
+    input wire [`ROP_LOGIC_OP_BITS-1:0] logic_op,
     input wire [31:0] src_color,
     input wire [31:0] dst_color,
-    input wire valid_in,
 
     // Outputs
-    output wire ready_out,
     output wire [31:0] color_out
 );
 
-    reg [31:0] result_color;
-
     always @(*) begin
-        case (rop_csrs.logic_op)
-            `ROP_LOGIC_OP_CLEAR:         result_color = 32'b0;
-            `ROP_LOGIC_OP_AND:           result_color = src_color & dst_color;
-            `ROP_LOGIC_OP_AND_REVERSE:   result_color = src_color & (~dst_color);
-            `ROP_LOGIC_OP_COPY:          result_color = src_color;
-            `ROP_LOGIC_OP_AND_INVERTED:  result_color = (~src_color) & dst_color;
-            `ROP_LOGIC_OP_NOOP:          result_color = dst_color;
-            `ROP_LOGIC_OP_XOR:           result_color = src_color ^ dst_color;
-            `ROP_LOGIC_OP_OR:            result_color = src_color | dst_color;
-            `ROP_LOGIC_OP_NOR:           result_color = ~(src_color | dst_color);
-            `ROP_LOGIC_OP_EQUIV:         result_color = ~(src_color ^ dst_color);
-            `ROP_LOGIC_OP_INVERT:        result_color = ~dst_color;
-            `ROP_LOGIC_OP_OR_REVERSE:    result_color = src_color | (~dst_color);
-            `ROP_LOGIC_OP_COPY_INVERTED: result_color = ~src_color;
-            `ROP_LOGIC_OP_OR_INVERTED:   result_color = (~src_color) | dst_color;
-            `ROP_LOGIC_OP_NAND:          result_color = ~(src_color & dst_color);
-            `ROP_LOGIC_OP_SET:           result_color = {32{1'b1}};
+        case (logic_op)
+            `ROP_LOGIC_OP_CLEAR:         color_out = 32'b0;
+            `ROP_LOGIC_OP_AND:           color_out = src_color & dst_color;
+            `ROP_LOGIC_OP_AND_REVERSE:   color_out = src_color & (~dst_color);
+            `ROP_LOGIC_OP_COPY:          color_out = src_color;
+            `ROP_LOGIC_OP_AND_INVERTED:  color_out = (~src_color) & dst_color;
+            `ROP_LOGIC_OP_NOOP:          color_out = dst_color;
+            `ROP_LOGIC_OP_XOR:           color_out = src_color ^ dst_color;
+            `ROP_LOGIC_OP_OR:            color_out = src_color | dst_color;
+            `ROP_LOGIC_OP_NOR:           color_out = ~(src_color | dst_color);
+            `ROP_LOGIC_OP_EQUIV:         color_out = ~(src_color ^ dst_color);
+            `ROP_LOGIC_OP_INVERT:        color_out = ~dst_color;
+            `ROP_LOGIC_OP_OR_REVERSE:    color_out = src_color | (~dst_color);
+            `ROP_LOGIC_OP_COPY_INVERTED: color_out = ~src_color;
+            `ROP_LOGIC_OP_OR_INVERTED:   color_out = (~src_color) | dst_color;
+            `ROP_LOGIC_OP_NAND:          color_out = ~(src_color & dst_color);
+            `ROP_LOGIC_OP_SET:           color_out = {32{1'b1}};
+            default:                     color_out = src_color;
         endcase
     end
-
-    wire        valid_out;
-    wire        store_result;
-
-    assign store_result = valid_in;
-
-    VX_pipe_register #(
-        .DATAW  (33),
-        .RESETW (1)
-    ) pipe_reg (
-        .clk      (clk),
-        .reset    (reset),
-        .enable   (store_result),
-        .data_in  ({valid_in,  result_color}),
-        .data_out ({valid_out, color_out})
-    );
-
-    assign ready_out = valid_out;
 
 endmodule
