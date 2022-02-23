cd06490a3fbae160606475ba42faa1e691834a63
Joshua Simpson <jsimps03@calpoly.edu>
2022-02-22 18:09:14 -0800

    Flushed out case statements for blend equations, missing logic still

--- hw/rtl/rop_unit/VX_rop_blend.sv
+++ hw/rtl/rop_unit/VX_rop_blend.sv
@@ -12,7 +12,10 @@ module VX_rop_blend #(
     input wire [:0]             mode_a,
 
     // CSR Parameters
-    rop_csrs_t reg_csrs,
+    rop_csrs_t                  reg_csrs,
+
+    // Logic Op enable (if enabled, blending is disabled even if blend enable is set)
+    input wire                  logic_op_enable,
 
     // Color Values (TODO: replace with REQs)
     input wire [31:0]           dst_color,
@@ -114,61 +117,99 @@ module VX_rop_blend #(
     end
 
     // Store the original source color in pipe_color in case blending is turned off
-    wire enable_out, reset_out;
+    wire enable_out, reset_out, logic_op_enable_out, stage1_pipe_en;
     wire [31:0] pipe_color;
 
+    // If either blending enable or logic op enable are set, store values in pipe_reg
+    assign stage1_pipe_en = enable || logic_op_enable;
+
     VX_pipe_register #(
-        .DATAW  (1 + 1 + (`ROP_BLEND_FACTOR_BITS * 4) + 32),
+        .DATAW  (1 + 1 + 1 + (`ROP_BLEND_FACTOR_BITS * 4) + 32),
         .RESETW (1)
     ) pipe_reg (
         .clk      (clk),
         .reset    (reset),
-        .enable   (enable),
-        .data_in  ({enable,     reset,     factor_src_rgb,      factor_rgb_a,      factor_dst_rgb,      factor_dst_a,      source_color}),
-        .data_out ({enable_out, reset_out, pipe_factor_src_rgb, pipe_factor_rgb_a, pipe_factor_dst_rgb, pipe_factor_dst_a, pipe_color})
+        .enable   (stage1_pipe_en),
+        .data_in  ({enable,     reset,     logic_op_enable,     factor_src_rgb,      factor_rgb_a,      factor_dst_rgb,      factor_dst_a,      source_color}),
+        .data_out ({enable_out, reset_out, logic_op_enable_out, pipe_factor_src_rgb, pipe_factor_rgb_a, pipe_factor_dst_rgb, pipe_factor_dst_a, pipe_color})
     );
+
+    wire [7:0] out_red, out_green, out_blue;
 
     always @(*) begin
         // Blend Equations (TODO: ask about logic op and whether blending is disabled or not when logic op is used as per GL spec)
         case (mode_rgb)
             `ROP_BLEND_MODE_FUNC_ADD: begin
-                out_red <=
-                out_blue <=
-                out_green <=
-            end
-            `ROP_BLEND_MODE_FUNC_SUBTRACT:
-            `ROP_BLEND_MODE_FUNC_REVERSE_SUBTRACT:
-            `ROP_BLEND_MODE_MIN:
-            `ROP_BLEND_MODE_MAX:
+                out_red   =
+                out_blue  =
+                out_green =
+                end
+            `ROP_BLEND_MODE_FUNC_SUBTRACT: begin
+                out_red   =
+                out_blue  =
+                out_green =
+                end
+            `ROP_BLEND_MODE_FUNC_REVERSE_SUBTRACT: begin
+                out_red   =
+                out_blue  =
+                out_green =
+                end
+            `ROP_BLEND_MODE_MIN: begin
+                out_red   =
+                out_blue  =
+                out_green =
+                end
+            `ROP_BLEND_MODE_MAX: begin
+                out_red   =
+                out_blue  =
+                out_green =
+                end
             `ROP_BLEND_MODE_LOGIC_OP:
-            default: 
+            default: begin
+                out_red   =
+                out_blue  =
+                out_green =
+                end
         endcase
         case (mode_a)
             `ROP_BLEND_MODE_FUNC_ADD: begin
-                out_a <=
-            end
-            `ROP_BLEND_MODE_FUNC_SUBTRACT:
-            `ROP_BLEND_MODE_FUNC_REVERSE_SUBTRACT:
-            `ROP_BLEND_MODE_MIN:
-            `ROP_BLEND_MODE_MAX:
+                out_a =
+                end
+            `ROP_BLEND_MODE_FUNC_SUBTRACT: begin
+                out_a =
+                end
+            `ROP_BLEND_MODE_FUNC_REVERSE_SUBTRACT: begin
+                out_a =
+                end
+            `ROP_BLEND_MODE_MIN: begin
+                out_a =
+                end
+            `ROP_BLEND_MODE_MAX: begin
+                out_a =
+                end
             `ROP_BLEND_MODE_LOGIC_OP:
-            default: 
+            default: begin
+                out_a =
+                end
         endcase
     end
 
     // If enable is off, pass the original source color (now stored in pipe_color) on to the next stage and ignore blending
-    wire [31:0] pipe_color_out;
-    assign pipe_color_out = (enable_out) ? {out_red, out_green, out_blue, out_a} : pipe_color;
+    wire stage2_pipe_en;
+    wire [31:0] pipe_out_color;
+
+    assign stage2_pipe_en = enable_out || logic_op_enable_out;
+    assign pipe_out_color = (enable_out || logic_op_enable_out) ? {out_red, out_green, out_blue, out_a} : pipe_color;
 
     VX_pipe_register #(
-        .DATAW  (1 + 1 + 32),
+        .DATAW  (32),
         .RESETW (1)
     ) pipe_reg (
         .clk      (clk),
         .reset    (reset_out),
-        .enable   (enable_out),
-        .data_in  ({enable_out, reset_out, pipe_color_out,      ,      ,      }),
-        .data_out ({          ,          , out_color,           ,      ,      })
+        .enable   (stage2_pipe_en),
+        .data_in  ({pipe_out_color}),
+        .data_out ({out_color})
     );
 
 endmodule
