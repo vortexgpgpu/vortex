bf80e6d90a79c73833102ee305cdffe5411e3b21
Santosh Srivatsan <santosh.srivatsan@gmail.com>
2022-03-04 14:30:56 -0500

    MSU basic unit test

--- hw/unit_tests/mem_streamer/memsim.cpp
+++ hw/unit_tests/mem_streamer/memsim.cpp
@@ -1,74 +1,104 @@
 #include <iostream>
-#include <map>
 #include <verilated.h>
-#include <verilated_vcd_c.h>
-#include "VVX_mem_streamer_test.h"
-#include "VVX_mem_streamer_test__Syms.h"
+#include "memsim.h"
 
 // Number of clock edges that we'll simulate
-#define MAX_SIM_TIME 300
-vluint64_t sim_time = 0;
-vluint64_t posedges = 0;
+#define SIM_TIME 50
 
-static uint64_t timestamp = 0;
 static bool trace_enabled = false;
 static uint64_t trace_start_time = 0;
 static uint64_t trace_stop_time = -1ull;
+static uint64_t timestamp = 0;
 
 double sc_time_stamp() { 
-  return timestamp;
+  	return timestamp;
 }
 
 bool sim_trace_enabled() {
-  if (timestamp >= trace_start_time 
-   && timestamp < trace_stop_time)
-    return true;
-  return trace_enabled;
+	if (timestamp >= trace_start_time 
+	&& timestamp < trace_stop_time)
+			return true;
+	return trace_enabled;
 }
 
 void sim_trace_enable (bool enable) {
-  trace_enabled = enable;
+  	trace_enabled = enable;
+}
+
+void generate_req(req_t *req) {
+	req->valid = 1;
+	req->rw = 0;
+	req->byteen = 1;
+	req->addr = 0x04030201;
+	req->data = 0x12345678;
+	req->tag = 0xAB;
+}
+
+MemSim::MemSim() {
+	msu_ = new VVX_mem_streamer_test();
+
+	// Enable tracing
+	Verilated::traceEverOn(true);
+    trace_ = new VerilatedVcdC;
+    msu_->trace(trace_, 99);
+    trace_->open("trace.vcd");
+}
+
+MemSim::~MemSim() {
+	trace_->close();
+	delete msu_;
+}
+
+void MemSim::eval() {
+	msu_->eval();
+	trace_->dump(timestamp++);
+}
+
+void MemSim::step() {
+	msu_->clk = 0;
+	this->eval();
+
+	msu_->clk = 1;
+	this->eval();
+}
+
+void MemSim::reset() {
+	msu_->reset = 1;
+	this->step();
+
+	msu_->reset = 0;
+	this->step();
+}
+
+void MemSim::set_core_req(req_t *core_req) {
+	msu_->req_valid 	= core_req->valid;
+	msu_->req_rw 		= core_req->rw;
+	msu_->req_mask 		= core_req->mask;
+	msu_->req_byteen 	= core_req->byteen;
+	msu_->req_addr 		= core_req->addr;
+	msu_->req_data 		= core_req->data;
+	msu_->req_tag 		= core_req->tag;
 }
 
 int main (int argc, char** argv, char** env) {
     Verilated::commandArgs(argc, argv);
-    VVX_mem_streamer_test *dut = new VVX_mem_streamer_test();
 
-    // Generate trace
-    Verilated::traceEverOn(true);
-    VerilatedVcdC *tfp = new VerilatedVcdC;
-    dut->trace(tfp, 99);
-    tfp->open("trace.vcd");
+	MemSim memsim;
+	req_t input;
 
-    // Simulate
-    while (sim_time < MAX_SIM_TIME) {
-        dut->reset = 0;
-        if (sim_time >=2 && sim_time <=4) {
-            dut->reset = 1;
-            dut->req_valid = 0;
-            dut->req_tag = 0x0;
-        }
-        dut->clk ^= 1;
-        
-        // Positive clock edge
-        if (1 == dut->clk) {
-            posedges++;
-            if (5 == posedges) {
-                dut->req_valid = 1;
-                dut->req_tag = 0x12345678;
-            } else {
-                dut->req_valid = 0;
-                dut->req_tag = 0;
-            }
-        }
+	generate_req(&input);
 
-        // Evaluate module
-        dut->eval();
-        tfp->dump(sim_time);
-        sim_time++;
-    }
-
-    tfp->close();
-    delete dut;
-    exit(EXIT_SUCCESS);
+	memsim.step();
+	memsim.reset();
+	memsim.set_core_req(&input);
+	memsim.step();
+	memsim.step();
+	memsim.step();
+	memsim.step();
+	memsim.step();
+	memsim.step();
+	memsim.step();
+	memsim.step();
+	
+	return 0;
 }
--- hw/unit_tests/mem_streamer/memsim.h
+++ hw/unit_tests/mem_streamer/memsim.h
@@ -4,49 +4,42 @@
 #include <map>
 #include <verilated.h>
 #include <verilated_vcd_c.h>
-#include "VVX_mem_streamer.h"
-#include "VVX_mem_streamer__Syms.h"
-
-
-#define NUM_REQS 4
-#define ADDRW 32
-#define DATAW 32
-#define TAGW 32
-#define WORD_SIZE 4
-#define QUEUE_SIZE 16
-#define QUEUE_ADDRW 4
-#define PARTIAL_RESPONSE
+#include "VVX_mem_streamer_test.h"
+#include "VVX_mem_streamer_test__Syms.h"
 
 // Input request
-struct Input {
+typedef struct {
     uint8_t valid;
     uint8_t rw;
     uint8_t mask;
     uint8_t byteen;
-    uint32_t* addr;
-    uint32_t* data;
+    uint32_t addr;
+    uint32_t data;
     uint32_t tag;
     uint8_t ready;
-};
+} req_t;
 
 // Output response
-struct Output{
+typedef struct {
     uint8_t valid;
     uint8_t mask;
     uint32_t* data;
     uint32_t tag;
     uint8_t ready;
-};
+} rsp_t;
 
 class MemSim {
+    private:
+        VVX_mem_streamer_test *msu_;
+        VerilatedVcdC *trace_;
+
+        void eval();
+
     public:
         MemSim();
         virtual ~MemSim();
 
-        void send_req(req_in_t *req);
-        void get_rsp(rsp_out_t *rsp);
-
-    private:
-        VVX_mem_streamer *msu_;
-        VerilatedVcdC *trace_;
+        void step();
+        void reset();
+        void set_core_req(req_t *req);
 };
