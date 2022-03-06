761db8908850d9531734250a031cdbf11e354b7b
Joshua Simpson <jsimps03@calpoly.edu>
2022-03-05 19:42:29 -0800

    Finished mult_add

--- hw/rtl/rop_unit/VX_rop_mult_add.sv
+++ hw/rtl/rop_unit/VX_rop_mult_add.sv
@@ -16,6 +16,73 @@ module VX_rop_mult_add #(
     output wire [31:0] color_out
 );
 
+    wire [7:0] src_red, src_green, src_blue, src_alpha, dst_red, dst_green, dst_blue, dst_alpha;
+    wire [7:0] src_blend_red, src_blend_green, src_blend_blue, src_blend_alpha, dst_blend_red, dst_blend_green, dst_blend_blue, dst_blend_alpha;
+    wire [15:0] red_combined, green_combined, blue_combined, alpha_combined;
+
+    assign src_red     = src_color[31:24];
+    assign src_green   = src_color[23:16];
+    assign src_blue    = src_color[15:8];
+    assign src_alpha   = src_color[7:0];
+
+    assign dst_red     = dst_color[31:24];
+    assign dst_green   = dst_color[23:16];
+    assign dst_blue    = dst_color[15:8];
+    assign dst_alpha   = dst_color[7:0];
+
+    assign src_blend_red     = src_blend_factor[31:24];
+    assign src_blend_green   = src_blend_factor[23:16];
+    assign src_blend_blue    = src_blend_factor[15:8];
+    assign src_blend_alpha   = src_blend_factor[7:0];
+
+    assign dst_blend_red     = dst_blend_factor[31:24];
+    assign dst_blend_green   = dst_blend_factor[23:16];
+    assign dst_blend_blue    = dst_blend_factor[15:8];
+    assign dst_blend_alpha   = dst_blend_factor[7:0];
+
+    always @(*) begin
+        // RGB blending
+        case(mode_rgb)
+            `ROP_BLEND_MODE_FUNC_ADD: begin
+                red_combined   = (src_red * src_blend_red) + (dst_red * dst_blend_red);
+                green_combined = (src_green * src_blend_green) + (dst_green * dst_blend_green);
+                blue_combined  = (src_blue * src_blend_blue) + (dst_blue * dst_blend_blue);
+            end
+            `ROP_BLEND_MODE_FUNC_SUBTRACT: begin
+                red_combined   = (src_red * src_blend_red) - (dst_red * dst_blend_red);
+                green_combined = (src_green * src_blend_green) - (dst_green * dst_blend_green);
+                blue_combined  = (src_blue * src_blend_blue) - (dst_blue * dst_blend_blue); 
+            end
+            `ROP_BLEND_MODE_FUNC_REVERSE_SUBTRACT: begin
+                red_combined   = (dst_red * src_blend_red) - (src_red * dst_blend_red);
+                green_combined = (dst_green * src_blend_green) - (src_green * dst_blend_green);
+                blue_combined  = (dst_blue * src_blend_blue) - (src_blue * dst_blend_blue);
+            end
+            default: begin
+                red_combined   = {src_red, src_red};
+                green_combined = {src_green, src_green};
+                blue_combined  = {src_blue, src_blue};
+            end
+        endcase
+        // Alpha blending
+        case(mode_a)
+            `ROP_BLEND_MODE_FUNC_ADD: begin
+                alpha_combined = (src_alpha * src_blend_alpha) + (dst_alpha * dst_blend_alpha);
+            end
+            `ROP_BLEND_MODE_FUNC_SUBTRACT: begin
+                alpha_combined = (src_alpha * src_blend_alpha) - (dst_alpha * dst_blend_alpha);
+            end
+            `ROP_BLEND_MODE_FUNC_REVERSE_SUBTRACT: begin
+                alpha_combined = (dst_alpha * src_blend_alpha) - (src_alpha * dst_blend_alpha);
+            end
+            default: begin
+                alpha_combined = {src_alpha, src_alpha};
+            end
+        endcase
+    end
+
+    assign color_out = {red_combined[15:8], green_combined[15:8], blue_combined[15:8], alpha_combined[15:8]};
+
     // How to do math because the color constant is float between [0, 1] and 
     // the RGBA values are 8 bit ints. Even if we do fixed point math, how is 
     // this accomplished? How many bits could the answer be and how do we trim 
