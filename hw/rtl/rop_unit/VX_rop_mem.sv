ddeda5d4dddf040b0f07af0b7e13673bedec4014
Santosh Srivatsan <santosh.srivatsan@gmail.com>
2022-03-16 13:15:03 -0400

    Minor update

--- hw/rtl/rop_unit/VX_rop_mem.sv
+++ hw/rtl/rop_unit/VX_rop_mem.sv
@@ -9,21 +9,21 @@ module VX_rop_mem #(
     input wire reset,
 
     // Input request
-    input wire                           req_valid,
-    input wire                           req_rw,
-    input wire [NUM_REQS-1:0]            req_mask,
-    input wire [WORD_SIZE-1:0]           req_byteen,
-    input wire [NUM_REQS-1:0][ADDRW-1:0] req_addr,
-    input wire [NUM_REQS-1:0][DATAW-1:0] req_data,
-    input wire [TAGW-1:0]                req_tag,
-    output wire                          req_ready,
+    input wire                       req_valid,
+    input wire                       req_rw,
+    input wire [NUM_LANES-1:0]       req_mask,
+    input wire [3:0]                 req_byteen,
+    input wire [NUM_LANES-1:0][31:0] req_addr,
+    input wire [NUM_LANES-1:0][31:0] req_data,
+    input wire [31:0]                req_tag,
+    output wire                      req_ready,
 
     // Output response
-    output wire                           rsp_valid,
-    output wire [NUM_REQS-1:0]            rsp_mask,
-    output wire [NUM_REQS-1:0][DATAW-1:0] rsp_data,
-    output wire [TAGW-1:0]                rsp_tag,
-    input wire                            rsp_ready
+    output wire                       rsp_valid,
+    output wire [NUM_LANES-1:0]       rsp_mask,
+    output wire [NUM_LANES-1:0][31:0] rsp_data,
+    output wire [31:0]                rsp_tag,
+    input wire                        rsp_ready
 );
 
     // Memory interface
@@ -69,7 +69,7 @@ module VX_rop_mem #(
         .mem_rsp_mask   (cache_rsp_if.mask),
         .mem_rsp_data   (cache_rsp_if.data),
         .mem_rsp_tag    (cache_rsp_if.tag),
-        .mem_rsp_ready  (cache_rsp_if.ready),
+        .mem_rsp_ready  (cache_rsp_if.ready)
     );
     
 endmodule
