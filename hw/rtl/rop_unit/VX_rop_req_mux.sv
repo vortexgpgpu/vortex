8f319f5924dcf5c7f0d4fc15b516d0d033bd8e32
Blaise Tine <tinebp@hotmail.com>
2022-03-24 01:49:23 -0400

    commenting out VX_rop_req_mux to fix build break!

--- hw/rtl/rop_unit/VX_rop_req_mux.sv
+++ hw/rtl/rop_unit/VX_rop_req_mux.sv
@@ -12,7 +12,32 @@ module VX_rop_req_mux #(
     // output request
     VX_rop_req_if.master    req_out_if
 );
+    `UNUSED_VAR (clk)
+    `UNUSED_VAR (reset)
 
+    // TODO
+    for (genvar i = 0; i < NUM_REQS; ++i) begin
+        wire valid = req_in_if[i].valid;
+        wire [`NUM_THREADS-1:0][`ROP_DIM_BITS-1:0] pos_x = req_in_if[i].pos_x;
+        wire [`NUM_THREADS-1:0][`ROP_DIM_BITS-1:0] pos_y = req_in_if[i].pos_y;
+        wire [`NUM_THREADS-1:0][31:0] color = req_in_if[i].color;
+        wire [`NUM_THREADS-1:0][`ROP_DEPTH_BITS-1:0] depth = req_in_if[i].depth;
+        `UNUSED_VAR (valid)
+        `UNUSED_VAR (pos_x)
+        `UNUSED_VAR (pos_y)
+        `UNUSED_VAR (color)
+        `UNUSED_VAR (depth)
+        assign req_in_if[i].ready = 0;
+    end
+
+    // TODO
+    assign req_out_if.valid = 0;
+    assign req_out_if.pos_x = 0;
+    assign req_out_if.pos_y = 0;
+    assign req_out_if.color = 0;
+    assign req_out_if.depth = 0;
+    `UNUSED_VAR (req_out_if.ready)
+/*
     // Local parameters to define bit widths and depth sizes based on number of threads and cores
     localparam ROP_FIFO_DATA_WIDTH = 1 + 2 * `NUM_THREADS + 2 * (`NUM_THREADS * `ROP_DIM_BITS) + (`NUM_THREADS * 32) + (`NUM_THREADS * `ROP_DEPTH_BITS);
     localparam ROP_FIFO_DEPTH = NUM_REQS * `NUM_THREADS;
@@ -95,5 +120,6 @@ module VX_rop_req_mux #(
     assign req_out_if.color = data_pop.color;
     assign req_out_if.depth = data_pop.depth;
     assign req_out_if.backface = data_pop.backface;
+*/
 
 endmodule
