1b59d895352d1878d82ee5506034397a28eb25a6
Blaise Tine <tinebp@iam-ssh1.research.intel-research.net>
2022-07-28 23:43:24 -0700

    minor updates

---
+++ hw/rtl/interfaces/VX_perf_gpu_if.sv
@@ -0,0 +1,51 @@
+`include "VX_define.vh"
+
+interface VX_perf_gpu_if ();
+
+`ifdef EXT_TEX_ENABLE
+    wire [`PERF_CTR_BITS-1:0] tex_stalls;
+`endif
+`ifdef EXT_RASTER_ENABLE
+    wire [`PERF_CTR_BITS-1:0] raster_stalls;
+`endif
+`ifdef EXT_ROP_ENABLE
+    wire [`PERF_CTR_BITS-1:0] rop_stalls;
+`endif
+`ifdef EXT_IMADD_ENABLE
+    wire [`PERF_CTR_BITS-1:0] imadd_stalls;
+`endif
+    wire [`PERF_CTR_BITS-1:0] wctl_stalls;
+
+    modport master (
+    `ifdef EXT_TEX_ENABLE
+        output tex_stalls,
+    `endif
+    `ifdef EXT_RASTER_ENABLE
+        output raster_stalls,
+    `endif
+    `ifdef EXT_ROP_ENABLE
+        output rop_stalls,
+    `endif
+    `ifdef EXT_IMADD_ENABLE
+        output imadd_stalls,       
+    `endif
+        output wctl_stalls
+    );
+
+    modport slave (
+    `ifdef EXT_TEX_ENABLE
+        input tex_stalls,
+    `endif
+    `ifdef EXT_RASTER_ENABLE
+        input raster_stalls,
+    `endif
+    `ifdef EXT_ROP_ENABLE
+        input rop_stalls,
+    `endif
+    `ifdef EXT_IMADD_ENABLE
+        input imadd_stalls,
+    `endif
+        input wctl_stalls
+    );
+
+endinterface
---
+++ hw/syn/altera/report_area.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Dump Area Report
+# first argument is the project name
+
+quartus_sh -t report_area.tcl afu_fit fit
