e8f74fe6969ad06b00e759d5b536f38f1c22c89b
Blaise Tine <tinebp@yahoo.com>
2022-08-28 21:37:38 -0400

    minor updates

--- hw/syn/altera/quartus/common.mk
+++ hw/syn/altera/quartus/common.mk
@@ -1,16 +1,15 @@
 RTL_DIR = ../../../../../rtl
 AFU_DIR = ../../../../../afu/opae
 THIRD_PARTY_DIR = ../../../../../../third_party
+IP_DIR = ../../../../../ip/altera;../../../../../ip/altera/$(DEVICE_FAMILY)
 
 ifeq ($(DEVICE_FAMILY), stratix10)
     FAMILY = "Stratix 10"
-	DEVICE = 1SX280HN2F43E2VG
-	IP_DIR = ../../../../../ip/altera/stratix10
+	DEVICE = 1SX280HN2F43E2VG	
 endif
 ifeq ($(DEVICE_FAMILY), arria10)
     FAMILY = "Arria 10"
 	DEVICE = 10AX115N3F40E2SG
-	IP_DIR = ../../../../../ip/altera/arria10
 endif
 
 CONFIGS += -set "NDEBUG"
