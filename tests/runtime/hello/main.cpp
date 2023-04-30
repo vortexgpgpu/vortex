907ece2226082cfe725291a3914e92352332ca45
shreenithi <72983224+wickedb@users.noreply.github.com>
2023-04-30 19:16:58 +0530

    Testing ror instrinsic

--- tests/runtime/hello/main.cpp
+++ tests/runtime/hello/main.cpp
@@ -1,9 +1,18 @@
+//this code tests the ROR intrinsic we added
 #include <stdio.h>
-#include <vx_print.h>
-
-int main()
+#include "vx_intrinsics.h"
+#include <stdint.h>
+int main(void)
 {
-	vx_printf("Hello World!\n");
-
-	return 0;
+        printf("Hello World!\n");
+        uint32_t result;
+        int x = 8, n=2; 
+        int y = (int)((unsigned)x >> n);
+        int z = x << (32 - n);
+        int g = y | z;
+        result = __intrin_rotr_imm(8, 2);
+        printf("Result = %ld and verif = %ld\n", result, g);
+        return 0;
 }
+
+
