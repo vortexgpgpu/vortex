5c39a114be429eb3472110b5821fa92ca25af433
Blaise Tine <tinebp@yahoo.com>
2023-05-15 19:51:20 -0400

    minor update

--- ci/travis_run.py
+++ ci/travis_run.py
@@ -23,12 +23,14 @@ def execute(command):
     process = subprocess.Popen(command, stdout=subprocess.PIPE)
     while True:
         output = process.stdout.readline()
-        if output == '' and process.poll() is not None:
-            break
         if output:
-            print output.strip()
-            sys.stdout.flush()
-    return process.returncode
+            line = output.decode('ascii').rstrip()
+            print(">>> " + line)
+            process.stdout.flush()
+        ret = process.poll()
+        if ret is not None:
+            return ret        
+    return -1
 
 def main(argv):
 
