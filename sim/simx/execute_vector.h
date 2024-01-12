a8fd56680fe52e7139d826ad72d0fee8fde90591
Martin Troiber <martin.troiber@gmail.com>
2024-01-12 12:30:57 +0000

    Separate out vector execution

---
+++ sim/simx/execute_vector.h
@@ -0,0 +1,3 @@
+#include "execute.h"
+
+void executeVector(const Instr &instr, std::vector<reg_data_t[3]> &rsdata, std::vector<reg_data_t> &rddata, std::vector<std::vector<Byte>> &vreg_file_, vtype vtype_, uint32_t vl_);
