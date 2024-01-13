4f73f7743d5ebc43b32bc67b86bb29fc1f56120c
Martin Troiber <martin.troiber@gmail.com>
2024-01-13 22:31:45 +0000

    Fix vadd.vi

--- sim/simx/execute.cpp
+++ sim/simx/execute.cpp
@@ -1420,7 +1420,7 @@ void Warp::execute(const Instr &instr, pipeline_trace_t *trace) {
     }
   } break;
   case VSET: {
-    executeVector(instr, core_, rsdata, rddata, vreg_file_, vtype_, vl_, warp_id_);
+    executeVector(instr, core_, rsdata, rddata, vreg_file_, vtype_, vl_, warp_id_, tmask_, num_threads);
   } break;    
   default:
     std::abort();
