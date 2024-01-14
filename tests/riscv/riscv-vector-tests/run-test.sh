e92291750601e07cc143aacd1faa540321eeffa9
Martin Troiber <martin.troiber@gmail.com>
2024-01-14 23:03:27 +0000

    Add precompiled vector tests

--- tests/riscv/riscv-vector-tests/run-test.sh
+++ tests/riscv/riscv-vector-tests/run-test.sh
@@ -10,13 +10,13 @@ else
 fi
 
 cd testcases
-rm "$1"*
-cp ../../../../third_party/riscv-vector-tests/out/v"$VLEN"x"$XLEN"machine/bin/stage2/"$1"* .
+rm "$1"*.elf "$1"*.bin "$1"*.dump "$1"*.log
+cp -f ../../../../third_party/riscv-vector-tests/out/v"$VLEN"x"$XLEN"machine/bin/stage2/"$1"* .
 passed=0
 failed=0
 all=0
 for f in "$1"* ; do 
-  mv "$f" "$f.elf";
+  ln -s "$f" "$f.elf";
   "$RISCV_TOOLCHAIN_PATH"/bin/riscv"$XLEN"-unknown-elf-objdump -D "$f.elf" > "$f.dump";
   "$RISCV_TOOLCHAIN_PATH"/bin/riscv"$XLEN"-unknown-elf-objcopy -O binary "$f.elf" "$f.bin";
   ../../../../sim/simx/simx -c 1 "$f.bin" &> "$f.log";
