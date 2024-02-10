3fee1a61935240e9145db0235addad9b632f7a5e
Blaise Tine <tinebp@yahoo.com>
2024-02-09 20:34:44 -0800

    minor update

--- ci/travis_run.py
+++ ci/travis_run.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-# Copyright © 2019-2023
-# 
+# Copyright 2019-2023
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -34,7 +34,7 @@ def monitor(stop):
             break
 
 def execute(command):
-    process = subprocess.Popen(command, stdout=subprocess.PIPE)
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     while True:
         output = process.stdout.readline()
         if output:
