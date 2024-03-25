86055335eefdc16f57a05b1101266335a8d04351
Blaise Tine <tinebp@yahoo.com>
2024-03-24 19:39:53 -0700

    minor update

--- ci/toolchain_env.sh
+++ ci/toolchain_env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright 2023 blaise
+# Copyright 2019-2023
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
