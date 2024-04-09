e500d4c2e07dc70d1e56d2556937b80b7ef9c12e
Blaise Tine <tinebp@yahoo.com>
2024-04-09 02:00:34 -0700

    CSR 32-bit/64-bit refactoring

--- hw/rtl/core/VX_csr_unit.sv
+++ hw/rtl/core/VX_csr_unit.sv
@@ -39,17 +39,17 @@ module VX_csr_unit import VX_gpu_pkg::*; #(
     `UNUSED_PARAM (CORE_ID)
     localparam PID_BITS   = `CLOG2(`NUM_THREADS / NUM_LANES);
     localparam PID_WIDTH  = `UP(PID_BITS);
-    localparam DATAW      = `UUID_WIDTH + `NW_WIDTH + NUM_LANES + `XLEN + `NR_BITS + 1 + NUM_LANES * 32 + PID_WIDTH + 1 + 1;
+    localparam DATAW      = `UUID_WIDTH + `NW_WIDTH + NUM_LANES + `XLEN + `NR_BITS + 1 + NUM_LANES * `XLEN + PID_WIDTH + 1 + 1;
 
     `UNUSED_VAR (execute_if.data.rs3_data)
     
-    reg [NUM_LANES-1:0][31:0]   csr_read_data;
-    reg  [31:0]                 csr_write_data;
-    wire [31:0]                 csr_read_data_ro, csr_read_data_rw;
-    wire [31:0]                 csr_req_data;
-    reg                         csr_rd_enable;
-    wire                        csr_wr_enable;
-    wire                        csr_req_ready;
+    reg [NUM_LANES-1:0][`XLEN-1:0]  csr_read_data;
+    reg  [`XLEN-1:0]                csr_write_data;
+    wire [`XLEN-1:0]                csr_read_data_ro, csr_read_data_rw;
+    wire [`XLEN-1:0]                csr_req_data;
+    reg                             csr_rd_enable;
+    wire                            csr_wr_enable;
+    wire                            csr_req_ready;
 
     // wait for all pending instructions to complete
     assign sched_csr_if.alm_empty_wid = execute_if.data.wid;
@@ -61,10 +61,10 @@ module VX_csr_unit import VX_gpu_pkg::*; #(
     wire [`VX_CSR_ADDR_BITS-1:0] csr_addr = execute_if.data.imm[`VX_CSR_ADDR_BITS-1:0];
     wire [`NRI_BITS-1:0] csr_imm = execute_if.data.imm[`VX_CSR_ADDR_BITS +: `NRI_BITS];
 
-    wire [NUM_LANES-1:0][31:0] rs1_data;
+    wire [NUM_LANES-1:0][`XLEN-1:0] rs1_data;
     `UNUSED_VAR (rs1_data)
     for (genvar i = 0; i < NUM_LANES; ++i) begin
-        assign rs1_data[i] = execute_if.data.rs1_data[i][31:0];
+        assign rs1_data[i] = execute_if.data.rs1_data[i][`XLEN-1:0];
     end
 
     wire csr_write_enable = (execute_if.data.op_type == `INST_SFU_CSRRW);
@@ -107,15 +107,15 @@ module VX_csr_unit import VX_gpu_pkg::*; #(
 
     // CSR read
 
-    wire [NUM_LANES-1:0][31:0] wtid, gtid;
+    wire [NUM_LANES-1:0][`XLEN-1:0] wtid, gtid;
 
     for (genvar i = 0; i < NUM_LANES; ++i) begin
         if (PID_BITS != 0) begin
-            assign wtid[i] = 32'(execute_if.data.pid * NUM_LANES + i);
+            assign wtid[i] = `XLEN'(execute_if.data.pid * NUM_LANES + i);
         end else begin
-            assign wtid[i] = 32'(i);
+            assign wtid[i] = `XLEN'(i);
         end
-        assign gtid[i] = (32'(CORE_ID) << (`NW_BITS + `NT_BITS)) + (32'(execute_if.data.wid) << `NT_BITS) + wtid[i];
+        assign gtid[i] = (`XLEN'(CORE_ID) << (`NW_BITS + `NT_BITS)) + (`XLEN'(execute_if.data.wid) << `NT_BITS) + wtid[i];
     end  
 
     always @(*) begin
@@ -132,8 +132,7 @@ module VX_csr_unit import VX_gpu_pkg::*; #(
 
     // CSR write
 
-    assign csr_req_data = execute_if.data.use_imm ? 32'(csr_imm) : rs1_data[0];
-
+    assign csr_req_data = execute_if.data.use_imm ? `XLEN'(csr_imm) : rs1_data[0];
     assign csr_wr_enable = (csr_write_enable || (| csr_req_data));
 
     always @(*) begin
@@ -155,9 +154,6 @@ module VX_csr_unit import VX_gpu_pkg::*; #(
     assign sched_csr_if.unlock_warp = csr_req_valid && csr_req_ready && execute_if.data.eop;
     assign sched_csr_if.unlock_wid = execute_if.data.wid;
 
-    // send response
-    wire [NUM_LANES-1:0][31:0] csr_commit_data;
-
     VX_elastic_buffer #(
         .DATAW (DATAW),
         .SIZE  (2)
@@ -166,14 +162,10 @@ module VX_csr_unit import VX_gpu_pkg::*; #(
         .reset     (reset),
         .valid_in  (csr_req_valid),
         .ready_in  (csr_req_ready),
-        .data_in   ({execute_if.data.uuid, execute_if.data.wid, execute_if.data.tmask, execute_if.data.PC, execute_if.data.rd, execute_if.data.wb, csr_read_data, execute_if.data.pid, execute_if.data.sop, execute_if.data.eop}),
-        .data_out  ({commit_if.data.uuid, commit_if.data.wid, commit_if.data.tmask, commit_if.data.PC, commit_if.data.rd, commit_if.data.wb, csr_commit_data, commit_if.data.pid, commit_if.data.sop, commit_if.data.eop}),
+        .data_in   ({execute_if.data.uuid, execute_if.data.wid, execute_if.data.tmask, execute_if.data.PC, execute_if.data.rd, execute_if.data.wb, csr_read_data,       execute_if.data.pid, execute_if.data.sop, execute_if.data.eop}),
+        .data_out  ({commit_if.data.uuid,  commit_if.data.wid,  commit_if.data.tmask,  commit_if.data.PC,  commit_if.data.rd,  commit_if.data.wb,  commit_if.data.data, commit_if.data.pid,  commit_if.data.sop,  commit_if.data.eop}),
         .valid_out (commit_if.valid),
         .ready_out (commit_if.ready)
     );
-    
-    for (genvar i = 0; i < NUM_LANES; ++i) begin
-        assign commit_if.data.data[i] = `XLEN'(csr_commit_data[i]);
-    end
 
 endmodule
