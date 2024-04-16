fee3a7c7158fce14e5391bfcaf8aed43f927c6bf
Xavier Caricchio <xavier.caricchio@gmail.com>
2024-04-16 12:06:50 +0200

    update makefile

--- tests/opengl/common.mk
+++ tests/opengl/common.mk
@@ -87,7 +87,7 @@ kernel.pocl: kernel.cl
 	$(CC) $(CXXFLAGS) -c $< -o $@
 
 $(PROJECT): $(OBJS)
-	$(CXX) $(CXXFLAGS) $^ $(LDFLAGS) $(OPENGL_PATH)/lib/GLSC2/glsc2.c.o -o $@
+	$(CXX) $(CXXFLAGS) $^ $(LDFLAGS) $(PROJECT_FLAGS) -o $@
 
 run-hostgpu: $(PROJECT) kernel.pocl
 	./$(PROJECT) $(OPTS)
