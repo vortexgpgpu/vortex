efc7a971dcb581992514c98205e4110256443dca
Blaise Tine <tinebp@yahoo.com>
2024-04-17 22:52:12 -0700

    minor update

--- kernel/scripts/vxbin.py
+++ kernel/scripts/vxbin.py
@@ -22,7 +22,7 @@ import re
 
 def get_vma_size(elf_file):
     try:
-        cmd = ['readelf', '-l', elf_file]
+        cmd = ['readelf', '-l', '-W', elf_file]
         process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
         output, errors = process.communicate()
         if process.returncode != 0:
