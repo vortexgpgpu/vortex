c554f53e44b5856a487f6b8e7331e0a2b5463693
Blaise Tine <tinebp@yahoo.com>
2024-04-26 18:03:30 -0700

    minor update

--- miscs/vortex.cmake
+++ miscs/vortex.cmake
@@ -10,8 +10,8 @@ set(CMAKE_SYSTEM_PROCESSOR riscv32)
 
 # Specify the binary utilities
 set(CMAKE_AR "${TOOLDIR}/llvm-vortex/bin/llvm-ar")
-set(CMAKE_ASM_COMPILER "${TOOLDIR}/llvm-vortex/bin/llvm-as")
-set(CMAKE_LINKER "${TOOLDIR}/llvm-vortex/bin/lld")
+#set(CMAKE_ASM_COMPILER "${TOOLDIR}/llvm-vortex/bin/llvm-as")
+set(CMAKE_LINKER "${TOOLDIR}/llvm-vortex/bin/llvm-lld")
 set(CMAKE_NM "${TOOLDIR}/llvm-vortex/bin/llvm-nm")
 set(CMAKE_RANLIB "${TOOLDIR}/llvm-vortex/bin/llvm-ranlib")
 
@@ -20,17 +20,17 @@ set(CMAKE_C_COMPILER "${TOOLDIR}/llvm-vortex/bin/clang")
 set(CMAKE_CXX_COMPILER "${TOOLDIR}/llvm-vortex/bin/clang++")
 
 # Compiler flags for C and C++
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -v --gcc-toolchain=${TOOLDIR}/riscv-gnu-toolchain -march=rv32imaf -mabi=ilp32f -Xclang -target-feature -Xclang +vortex -mcmodel=medany -fno-rtti -fno-exceptions -fdata-sections -ffunction-sections")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -v --gcc-toolchain=${TOOLDIR}/riscv-gnu-toolchain -march=rv32imaf -mabi=ilp32f -Xclang -target-feature -Xclang +vortex -mcmodel=medany -fno-rtti -fno-exceptions -fdata-sections -ffunction-sections")
+set(CMAKE_C_FLAGS "-v --gcc-toolchain=${TOOLDIR}/riscv-gnu-toolchain -march=rv32imaf -mabi=ilp32f -Xclang -target-feature -Xclang +vortex -mcmodel=medany -fno-rtti -fno-exceptions -fdata-sections -ffunction-sections")
+set(CMAKE_CXX_FLAGS "-v --gcc-toolchain=${TOOLDIR}/riscv-gnu-toolchain -march=rv32imaf -mabi=ilp32f -Xclang -target-feature -Xclang +vortex -mcmodel=medany -fno-rtti -fno-exceptions -fdata-sections -ffunction-sections")
 
 # Set the sysroot
-set(CMAKE_SYSROOT "${TOOLDIR}/riscv-gnu-toolchain/riscv32-unknown-elf")
+set(CMAKE_SYSROOT "${TOOLDIR}/riscv32-gnu-toolchain/riscv32-unknown-elf")
 
 # Linker flags
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fuse-ld=lld -nostartfiles -Wl,-Bstatic,--gc-sections,-T,${VORTEX_HOME}/kernel/scripts/link32.ld,--defsym=STARTUP_ADDR=${STARTUP_ADDR} ${VORTEX_BUILD}/kernel/libvortexrt.a")
+set(CMAKE_EXE_LINKER_FLAGS "-fuse-ld=lld -nostartfiles -Wl,-Bstatic,--gc-sections,-T,${VORTEX_HOME}/kernel/scripts/link32.ld ${VORTEX_BUILD}/kernel/libvortexrt.a")
 
 # Don't run the linker on compiler check
-#set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
 set(HAVE_CXX_ATOMICS64_WITHOUT_LIB True)
 set(HAVE_CXX_ATOMICS_WITHOUT_LIB True)
