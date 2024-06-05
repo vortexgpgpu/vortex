b4866e9ba588c4dfc39edfa1b2d96c5d6056af84
XaviCH <xavier.caricchio@gmail.com>
2024-06-05 23:39:05 +0200

    fix test

--- tests/opengl/fragment/main.cc
+++ tests/opengl/fragment/main.cc
@@ -15,25 +15,23 @@
 #define WIDTH 700
 #define HEIGHT 700
 
-int main (int argc, char **argv) {
-  // parse command arguments
-  cl_platform_id platform_id;
-  cl_device_id device_id;
-  cl_context context;
-  cl_kernel kernel;
-  cl_program program;
-  cl_mem m_image, gl_FragCoord, gl_Rasterization, gl_Discard, gl_FragColor;
-  cl_int _err;
-  size_t kernel_size;
+cl_platform_id platform_id;
+cl_device_id device_id;
+cl_context context;
+cl_kernel kernel;
+cl_program program;
+cl_mem m_image, gl_FragCoord, gl_Rasterization, gl_Discard, gl_FragColor;
+cl_int _err;
+size_t kernel_size;
 
-  printf("Hey\n");
+int main (int argc, char **argv) {
+
   // Getting platform and device information
   CL_CHECK(clGetPlatformIDs(1, &platform_id, NULL));
   CL_CHECK(clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, NULL));
 
-  printf("Create context\n");
   context = CL_CHECK2(clCreateContext(NULL, 1, &device_id, NULL, NULL,  &_err));
-  printf("Create image\n");
+
   PPMImage* image = readPPM("dog.ppm");
   uint8_t* data = (uint8_t*) malloc(image->x*image->y*sizeof(uint8_t[4]));
   for(uint32_t i=0; i<image->x*image->y; ++i) {
@@ -42,21 +40,21 @@ int main (int argc, char **argv) {
     data[i*4+2] = image->data[i].blue;
     data[i*4+3] = 0xFFu;
   }
-  printf("Allocate device buffers\n");
   m_image = CL_CHECK2(clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, image->x*image->y*sizeof(uint8_t[4]), data, &_err));
   
-  float* data_coord = (float*)malloc(WIDTH*HEIGHT*4);
-  for(int w=0; w<WIDTH; ++w) {
-    for(int h=0; h<HEIGHT; ++h) {
-      data_coord[w*HEIGHT*4+h*4] = w;
-      data_coord[w*HEIGHT*4+h*4+1] = h;
+  float* data_coord = (float*)malloc(WIDTH*HEIGHT*sizeof(float[4]));
+  for(int h=0; h<HEIGHT; ++h) {
+    for(int w=0; w<WIDTH; ++w) {
+      float *coord = data_coord + (h*WIDTH+w)*sizeof(float[4]);
+      coord[0] = w;
+      coord[1] = h;
     }
   }
   gl_Rasterization = CL_CHECK2(clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, WIDTH*HEIGHT*sizeof(float[4]), data_coord, &_err));
   gl_FragColor = CL_CHECK2(clCreateBuffer(context, CL_MEM_WRITE_ONLY, WIDTH*HEIGHT*sizeof(float[4]), NULL, &_err));
-
-  gl_FragCoord = CL_CHECK2(clCreateBuffer(context, CL_MEM_READ_ONLY, 1, NULL, &_err));
-  gl_Discard = CL_CHECK2(clCreateBuffer(context, CL_MEM_READ_ONLY, 1, NULL, &_err));
+  // unused buffers
+  gl_FragCoord = CL_CHECK2(clCreateBuffer(context, CL_MEM_READ_ONLY, WIDTH*HEIGHT*sizeof(float[4]), NULL, &_err));
+  gl_Discard = CL_CHECK2(clCreateBuffer(context, CL_MEM_READ_ONLY, WIDTH*HEIGHT*sizeof(uint8_t), NULL, &_err));
 
   uint8_t* kernel_bin;
   printf("Create program from kernel source\n");
