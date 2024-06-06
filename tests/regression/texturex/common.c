60700eac92ca74641525bb18b4e35d1d75e65754
Xavier Caricchio <xavier.caricchio@gmail.com>
2024-06-06 19:13:43 +0000

    fix

--- tests/regression/texturex/common.c
+++ tests/regression/texturex/common.c
@@ -1,10 +1,7 @@
-#include <GLSC2/glsc2.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 #include <math.h>
-#include <CL/opencl.h>
-#include <GLSC2/glsc2.h>
 #include <unistd.h> 
 #include <string.h>
 
@@ -31,67 +28,6 @@ static int read_kernel_file(const char* filename, uint8_t** data, size_t* size) 
   return 0;
 }
 
-
-/** Context utils **\
- * 
-*/
-typedef struct {
-    GLuint colorbuffer, depthbuffer, stencilbuffer;
-} RENDERBUFFERS;
-
-typedef struct {
-    GLuint framebuffers[2]; // buffers to be swapped
-    RENDERBUFFERS renderbuffers[2]; // renderbuffer associeted
-    GLsizei activated; // framebuffer active
-} CONTEXT;
-
-void createContext(CONTEXT* display, GLuint width, GLuint height) {
-
-    // Gen framebuffers & renderbuffers
-    glGenFramebuffers(2, display->framebuffers);
-    glGenRenderbuffers(1, &display->renderbuffers[0].colorbuffer);
-    glGenRenderbuffers(1, &display->renderbuffers[0].depthbuffer);
-    glGenRenderbuffers(1, &display->renderbuffers[0].stencilbuffer);
-    glGenRenderbuffers(1, &display->renderbuffers[1].colorbuffer);
-    glGenRenderbuffers(1, &display->renderbuffers[1].depthbuffer);
-    glGenRenderbuffers(1, &display->renderbuffers[1].stencilbuffer);
-
-    // Framebuffer0
-    glBindFramebuffer(GL_FRAMEBUFFER, display->framebuffers[0]);
-    // Build the renderbuffers
-    glBindRenderbuffer(GL_RENDERBUFFER, display->renderbuffers[0].colorbuffer);
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_RGBA4, width, height);
-    glBindRenderbuffer(GL_RENDERBUFFER, display->renderbuffers[0].depthbuffer);
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, width, height);
-    glBindRenderbuffer(GL_RENDERBUFFER, display->renderbuffers[0].stencilbuffer);
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_STENCIL_INDEX8, width, height);
-    // Associate buffers
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, display->renderbuffers[0].colorbuffer);
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, display->renderbuffers[0].depthbuffer);
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, display->renderbuffers[0].stencilbuffer);
-
-    // Framebuffer1
-    glBindFramebuffer(GL_FRAMEBUFFER, display->framebuffers[1]);
-    // Build the renderbuffers
-    glBindRenderbuffer(GL_RENDERBUFFER, display->renderbuffers[1].colorbuffer);
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_RGBA4, width, height);
-    glBindRenderbuffer(GL_RENDERBUFFER, display->renderbuffers[1].depthbuffer);
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, width, height);
-    glBindRenderbuffer(GL_RENDERBUFFER, display->renderbuffers[1].stencilbuffer);
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_STENCIL_INDEX8, width, height);
-    // Associate buffers
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, display->renderbuffers[1].colorbuffer);
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, display->renderbuffers[1].depthbuffer);
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, display->renderbuffers[1].stencilbuffer);
-}
-
-void swapBuffers(CONTEXT* display) {
-    GLsizei to_activate = 1;
-    if (display->activated == 1) to_activate = 0;
-
-    glBindFramebuffer(GL_FRAMEBUFFER ,display->framebuffers[to_activate]);
-    display->activated = to_activate;
-}
 
 /* Image utils */
 void printPPM(const char* filename, size_t width, size_t height, const uint8_t *data) {
