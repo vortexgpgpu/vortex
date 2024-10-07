c3ae793cdf6f58b911c2d2d9082f4717442911ca
MichaelJSr <miky.srouji@gmail.com>
2024-10-07 14:15:00 -0700

    re-added riscv-vector-tests option to simx

--- sim/simx/main.cpp
+++ sim/simx/main.cpp
@@ -36,6 +36,7 @@ uint32_t num_threads = NUM_THREADS;
 uint32_t num_warps = NUM_WARPS;
 uint32_t num_cores = NUM_CORES;
 bool showStats = false;
+bool riscv_test = false;
 const char* program = nullptr;
 
 static void parse_args(int argc, char **argv) {
@@ -53,6 +54,9 @@ static void parse_args(int argc, char **argv) {
         break;
       case 's':
         showStats = true;
+        break;
+      case 'r':
+        riscv_test = true;
         break;
     	case 'h':
       	show_usage();
@@ -121,5 +125,5 @@ int main(int argc, char **argv) {
     ram.read(&exitcode, (IO_MPM_ADDR + 8), 4);
   }
 
-  return exitcode;
+  return exitcode if not riscv_test else (1 - exitcode);
 }
