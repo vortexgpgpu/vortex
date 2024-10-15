645befdce6ebe6b06438a8a0ea632da1f8860cdf
Blaise Tine <tinebp@yahoo.com>
2024-10-15 11:23:29 -0700

    minor update

--- hw/rtl/cache/VX_cache_repl.sv
+++ hw/rtl/cache/VX_cache_repl.sv
@@ -21,9 +21,9 @@ module plru_decoder #(
     parameter WAY_IDX_BITS  = $clog2(NUM_WAYS),
     parameter WAY_IDX_WIDTH = `UP(WAY_IDX_BITS)
 ) (
-    input wire [WAY_IDX_WIDTH-1:0] way_idx,
-    input wire [`UP(NUM_WAYS-1)-1:0] lru_in,
-    output wire [`UP(NUM_WAYS-1)-1:0] lru_out
+    input wire [WAY_IDX_WIDTH-1:0]    way_idx,
+    output wire [`UP(NUM_WAYS-1)-1:0] lru_data,
+    output wire [`UP(NUM_WAYS-1)-1:0] lru_mask
 );
     if (NUM_WAYS > 1) begin : g_dec
         wire [`UP(NUM_WAYS-1)-1:0] data;
@@ -40,11 +40,12 @@ module plru_decoder #(
             end
             assign data[i] = ~way_idx[WAY_IDX_BITS-$clog2(i+2)];
         end
-        assign lru_out = (data & mask) | (lru_in & ~mask);
+        assign lru_data = data;
+        assign lru_mask = mask;
     end else begin : g_no_dec
         `UNUSED_VAR (way_idx)
-        `UNUSED_VAR (lru_in)
-        assign lru_out = '0;
+        assign lru_data = '0;
+        assign lru_mask = '0;
     end
 
 endmodule
@@ -60,13 +61,17 @@ module plru_encoder #(
     if (NUM_WAYS > 1) begin : g_enc
         wire [WAY_IDX_BITS-1:0] tmp;
         for (genvar i = 0; i < WAY_IDX_BITS; ++i) begin : g_i
-            VX_mux #(
-                .N (2**i)
-            ) mux (
-                .data_in  (lru_in[((2**i)-1)+:(2**i)]),
-                .sel_in   (tmp[WAY_IDX_BITS-1-:i]),
-                .data_out (tmp[WAY_IDX_BITS-1-i])
-            );
+            if (i == 0) begin : g_i_0
+                assign tmp[WAY_IDX_WIDTH-1] = lru_in[0];
+            end else begin : g_i_n
+                VX_mux #(
+                    .N (2**i)
+                ) mux (
+                    .data_in  (lru_in[((2**i)-1)+:(2**i)]),
+                    .sel_in   (tmp[WAY_IDX_BITS-1-:i]),
+                    .data_out (tmp[WAY_IDX_BITS-1-i])
+                );
+            end
         end
         assign way_idx = tmp;
     end else begin : g_no_enc
@@ -104,24 +109,29 @@ module VX_cache_repl #(
 
     if (REPL_POLICY == `CS_REPL_PLRU) begin : g_plru
         // Pseudo Least Recently Used replacement policy
-        localparam LRU_WIDTH = NUM_WAYS-1;
-        `UNUSED_VAR (repl_valid)
-
-        reg [`UP(LRU_WIDTH)-1:0] plru_tree [0:`CS_LINES_PER_BANK-1];
+        localparam LRU_WIDTH = `UP(NUM_WAYS-1);
 
         wire [WAY_IDX_WIDTH-1:0] repl_way_idx;
         wire [WAY_IDX_WIDTH-1:0] hit_way_idx;
-        wire [`UP(LRU_WIDTH)-1:0] plru_update;
+        wire [LRU_WIDTH-1:0] plru_rdata;
+        wire [LRU_WIDTH-1:0] plru_wdata;
+        wire [LRU_WIDTH-1:0] plru_wmask;
 
-        always @(posedge clk) begin
-            if (reset) begin
-                plru_tree <= '0;
-            end else begin
-                if (hit_valid) begin
-                    plru_tree[hit_line] <= plru_update;
-                end
-            end
-        end
+        VX_dp_ram #(
+            .DATAW  (LRU_WIDTH),
+            .SIZE   (`CS_LINES_PER_BANK),
+            .WRENW  (LRU_WIDTH)
+        ) plru_store (
+            .clk   (clk),
+            .reset (reset),
+            .read  (repl_valid),
+            .write (hit_valid),
+            .wren  (plru_wmask),
+            .waddr (hit_line),
+            .raddr (repl_line),
+            .wdata (plru_wdata),
+            .rdata (plru_rdata)
+        );
 
         VX_onehot_encoder #(
             .N (NUM_WAYS)
@@ -134,15 +144,15 @@ module VX_cache_repl #(
         plru_decoder #(
             .NUM_WAYS (NUM_WAYS)
         ) plru_dec (
-            .way_idx (hit_way_idx),
-            .lru_in  (plru_tree[hit_line]),
-            .lru_out (plru_update)
+            .way_idx  (hit_way_idx),
+            .lru_data (plru_wdata),
+            .lru_mask (plru_wmask)
         );
 
         plru_encoder #(
             .NUM_WAYS (NUM_WAYS)
         ) plru_enc (
-            .lru_in  (plru_tree[repl_line]),
+            .lru_in  (plru_rdata),
             .way_idx (repl_way_idx)
         );
 
@@ -160,16 +170,28 @@ module VX_cache_repl #(
         `UNUSED_VAR (hit_valid)
         `UNUSED_VAR (hit_line)
         `UNUSED_VAR (hit_way)
-        reg [`UP(CTR_WIDTH)-1:0] counters [0:`CS_LINES_PER_BANK-1];
-        always @(posedge clk) begin
-            if (repl_valid) begin
-                counters[repl_line] <= counters[repl_line] + 1;
-            end
-        end
+
+        wire [`UP(CTR_WIDTH)-1:0] ctr_rdata;
+        wire [`UP(CTR_WIDTH)-1:0] ctr_wdata = ctr_rdata + 1;
+
+        VX_sp_ram #(
+            .DATAW (`UP(CTR_WIDTH)),
+            .SIZE  (`CS_LINES_PER_BANK)
+        ) ctr_store (
+            .clk   (clk),
+            .reset (reset),
+            .read  (repl_valid),
+            .write (repl_valid),
+            .wren  (1'b1),
+            .addr  (repl_line),
+            .wdata (ctr_wdata),
+            .rdata (ctr_rdata)
+        );
+
         VX_decoder #(
             .N (WAY_IDX_BITS)
         ) ctr_decoder (
-            .sel_in   (counters[repl_line]),
+            .sel_in   (ctr_rdata),
             .data_in  (1'b1),
             .data_out (repl_way)
         );
