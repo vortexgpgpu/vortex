3ce16c5546fd0c5938bd8af2d9448042f520063b
MichaelJSr <miky.srouji@gmail.com>
2025-02-17 16:55:24 -0800

    Add compilation flags for vector extension

--- tests/regression/common.mk
+++ tests/regression/common.mk
@@ -9,10 +9,19 @@ VORTEX_RT_PATH ?= $(ROOT_DIR)/runtime
 VORTEX_KN_PATH ?= $(ROOT_DIR)/kernel
 
 ifeq ($(XLEN),64)
+ifeq ($(EXT_V_ENABLE),1)
+VX_CFLAGS += -march=rv64imafdv_zve64d -mabi=lp64d # Compile for vector extension
+else
 VX_CFLAGS += -march=rv64imafd -mabi=lp64d
+endif
 STARTUP_ADDR ?= 0x180000000
+
+else
+ifeq ($(EXT_V_ENABLE),1)
+VX_CFLAGS += -march=rv32imafv_zve32f -mabi=ilp32f # Compile for vector extension
 else
 VX_CFLAGS += -march=rv32imaf -mabi=ilp32f
+endif
 STARTUP_ADDR ?= 0x80000000
 endif
 
