40d4a3d10439d67143fbb312d1bc1c31a7019ff1
tinebp <tinebp@yahoo.com>
2025-05-09 10:59:11 -0700

    simobject api refactoring

--- sim/common/simobject.h
+++ sim/common/simobject.h
@@ -21,10 +21,12 @@
 #include <queue>
 #include <assert.h>
 #include "mempool.h"
+#include "util.h"
+#include "linked_list.h"
+
+namespace vortex {
 
 class SimObjectBase;
-
-///////////////////////////////////////////////////////////////////////////////
 
 class SimPortBase {
 public:
@@ -34,14 +36,43 @@ public:
     return module_;
   }
 
+  SimPortBase* sink() const {
+    return sink_;
+  }
+
+  SimPortBase* source() const {
+    return source_;
+  }
+
+  virtual bool empty() const = 0;
+
+  virtual bool full() const = 0;
+
+  virtual uint32_t size() const = 0;
+
+  virtual uint32_t capacity() const = 0;
+
 protected:
-  SimPortBase(SimObjectBase* module)
+  SimPortBase(SimObjectBase* module, uint32_t capacity)
     : module_(module)
+    , capacity_(capacity)
+    , sink_(nullptr)
+    , source_(nullptr)
   {}
+
+  virtual void do_pop() = 0;
 
   SimPortBase& operator=(const SimPortBase&) = delete;
 
   SimObjectBase* module_;
+  uint32_t       capacity_;
+  SimPortBase*   sink_;
+  SimPortBase*   source_;
+
+  LinkedListNode<SimPortBase> pop_list_;
+  LinkedListNode<SimPortBase> push_list_;
+
+  friend class SimPlatform;
 };
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -51,57 +82,95 @@ class SimPort : public SimPortBase {
 public:
   typedef std::function<void (const Pkt&, uint64_t)> TxCallback;
 
-  SimPort(SimObjectBase* module)
-    : SimPortBase(module)
-    , sink_(nullptr)
+  SimPort(SimObjectBase* module, uint32_t capacity = 0)
+    : SimPortBase(module, capacity)
     , tx_cb_(nullptr)
   {}
 
   void bind(SimPort<Pkt>* sink) {
+    __assert(0 == capacity_, "only virtual ports can be used a link!")
     assert(sink_ == nullptr);
+    sink->source_ = this;
     sink_ = sink;
+    sink_transfer_ = nullptr;
+  }
+
+  template <typename U>
+  void bind(SimPort<U>* sink) {
+    __assert(0 == capacity_, "only virtual ports can be used a link!")
+    assert(sink_ == nullptr);
+    sink->source_ = this;
+    sink_ = sink;
+    sink_transfer_ = [sink](const Pkt& pkt, uint64_t cycles) {
+      sink->transfer(static_cast<U>(pkt), cycles);
+    };
+  }
+
+  template <typename U, typename Converter>
+  void bind(SimPort<U>* sink, const Converter& converter) {
+    __assert(0 == capacity_, "only virtual ports can be used a link!")
+    assert(sink_ == nullptr);
+    sink->source_ = this;
+    sink_ = sink;
+    sink_transfer_ = [sink, converter](const Pkt& pkt, uint64_t cycles) {
+      sink->transfer(static_cast<U>(converter(pkt)), cycles);
+    };
   }
 
   void unbind() {
-    sink_ = nullptr;
+    if (sink_) {
+      sink_->source_ = nullptr;
+      sink_ = nullptr;
+      sink_transfer_ = nullptr;
+    }
   }
 
-  bool connected() const {
-    return (sink_ != nullptr);
-  }
-
-  SimPort* sink() const {
-    return sink_;
-  }
-
-  bool empty() const {
+  bool empty() const override {
+    if (sink_) {
+      return sink_->empty();
+    }
     return queue_.empty();
   }
 
+  bool full() const override {
+    if (sink_) {
+      return sink_->full();
+    }
+    return (capacity_ != 0 && queue_.size() >= capacity_);
+  }
+
+  uint32_t size() const override {
+    if (sink_) {
+      return sink_->size();
+    }
+    return queue_.size();
+  }
+
+  uint32_t capacity() const override {
+    if (sink_) {
+      return sink_->capacity();
+    }
+    return capacity_;
+  }
+
   const Pkt& front() const {
+    __assert(sink_ == nullptr, "cannot be called on a stub port!")
+    __assert(!this->empty(), "port is empty!");
     return queue_.front();
   }
 
   Pkt& front() {
+    __assert(sink_ == nullptr, "cannot be called on a stub port!")
+    __assert(!this->empty(), "port is empty!");
     return queue_.front().pkt;
   }
 
-  void push(const Pkt& pkt, uint64_t delay = 1) const;
+  void push(const Pkt& pkt, uint64_t delay = 1);
 
-  uint64_t pop() {
-    auto cycles = queue_.front().cycles;
-    queue_.pop();
-    return cycles;
-  }
+  uint64_t pop();
 
   void tx_callback(const TxCallback& callback) {
     tx_cb_ = callback;
-  }
-
-  uint64_t arrival_time() const {
-    if (queue_.empty())
-      return 0;
-    return queue_.front().cycles;
   }
 
 protected:
@@ -111,23 +180,32 @@ protected:
   };
 
   std::queue<timed_pkt_t> queue_;
-  SimPort*   sink_;
   TxCallback tx_cb_;
+  TxCallback sink_transfer_;
 
-  void transfer(const Pkt& data, uint64_t cycles) {
+  void transfer(const Pkt& pkt, uint64_t cycles) {
     if (tx_cb_) {
-      tx_cb_(data, cycles);
+      tx_cb_(pkt, cycles);
     }
     if (sink_) {
-      sink_->transfer(data, cycles);
+      if (sink_transfer_) {
+        sink_transfer_(pkt, cycles);
+      } else {
+        reinterpret_cast<SimPort<Pkt>*>(sink_)->transfer(pkt, cycles);
+      }
     } else {
-      queue_.push({data, cycles});
+      queue_.push({pkt, cycles});
     }
+  }
+
+  void do_pop() override {
+    queue_.pop();
   }
 
   SimPort& operator=(const SimPort&) = delete;
 
   template <typename U> friend class SimPortEvent;
+  template <typename U> friend class SimPort;
 };
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -167,23 +245,10 @@ public:
     , pkt_(pkt)
   {}
 
-  void* operator new(size_t /*size*/) {
-    return allocator_.allocate();
-  }
-
-  void operator delete(void* ptr) {
-    allocator_.deallocate(ptr);
-  }
-
 protected:
   Func func_;
   Pkt  pkt_;
-
-  static MemoryPool<SimCallEvent<Pkt>> allocator_;
 };
-
-template <typename Pkt>
-MemoryPool<SimCallEvent<Pkt>> SimCallEvent<Pkt>::allocator_(64);
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -200,27 +265,21 @@ public:
     , pkt_(pkt)
   {}
 
-  void* operator new(size_t /*size*/) {
-    return allocator_.allocate();
-  }
-
-  void operator delete(void* ptr) {
-    allocator_.deallocate(ptr);
-  }
-
 protected:
   const SimPort<Pkt>* port_;
   Pkt pkt_;
-
-  static MemoryPool<SimPortEvent<Pkt>> allocator_;
 };
-
-template <typename Pkt>
-MemoryPool<SimPortEvent<Pkt>> SimPortEvent<Pkt>::allocator_(64);
 
 ///////////////////////////////////////////////////////////////////////////////
 
-class SimContext;
+class SimContext {
+private:
+  SimContext() {}
+
+  friend class SimPlatform;
+};
+
+///////////////////////////////////////////////////////////////////////////////
 
 class SimObjectBase {
 public:
@@ -234,16 +293,17 @@ public:
 
 protected:
 
-  SimObjectBase(const SimContext& ctx, const std::string& name);
+  SimObjectBase(const SimContext&, const std::string& name) : name_(name) {}
 
 private:
+
+  std::string name_;
 
   virtual void do_reset() = 0;
 
   virtual void do_tick() = 0;
 
-  std::string name_;
-
+  friend class SimPortBase;
   friend class SimPlatform;
 };
 
@@ -282,13 +342,6 @@ private:
   }
 };
 
-class SimContext {
-private:
-  SimContext() {}
-
-  friend class SimPlatform;
-};
-
 ///////////////////////////////////////////////////////////////////////////////
 
 class SimPlatform {
@@ -304,7 +357,7 @@ public:
   }
 
   void finalize() {
-    instance().clear();
+    instance().cleanup();
   }
 
   template <typename Impl, typename... Args>
@@ -323,7 +376,8 @@ public:
                 const Pkt& pkt,
                 uint64_t delay) {
     assert(delay != 0);
-    auto evt = std::make_shared<SimCallEvent<Pkt>>(callback, pkt, cycles_ + delay);
+    static PoolAllocator<SimCallEvent<Pkt>, 64> s_allocator;
+    auto evt = std::allocate_shared<SimCallEvent<Pkt>>(s_allocator, callback, pkt, cycles_ + delay);
     events_.emplace_back(evt);
   }
 
@@ -336,7 +390,7 @@ public:
   }
 
   void tick() {
-    // evaluate events
+    // fire events
     auto evt_it = events_.begin();
     auto evt_it_end = events_.end();
     while (evt_it != evt_it_end) {
@@ -348,10 +402,16 @@ public:
         ++evt_it;
       }
     }
-    // evaluate components
-    for (auto& object : objects_) {
+    // execute objects
+    for (auto object : objects_) {
       object->do_tick();
     }
+    // realize objects
+    for (auto it = pop_list_.begin(); it != pop_list_.end();) {
+      it->do_pop();
+      it = pop_list_.erase(it);
+    }
+    push_list_.clear();
     // advance clock
     ++cycles_;
   }
@@ -365,34 +425,61 @@ private:
   SimPlatform() : cycles_(0) {}
 
   virtual ~SimPlatform() {
-    this->clear();
+    this->cleanup();
   }
 
-  void clear() {
+  void cleanup() {
     objects_.clear();
     events_.clear();
   }
 
   template <typename Pkt>
-  void schedule(const SimPort<Pkt>* port, const Pkt& pkt, uint64_t delay) {
+  void schedule_push(SimPort<Pkt>* port, const Pkt& pkt, uint64_t delay) {
     assert(delay != 0);
-    auto evt = SimEventBase::Ptr(new SimPortEvent<Pkt>(port, pkt, cycles_ + delay));
+    if (port->capacity() != 0) {
+      __assert(0 == push_list_.count(port), "cannot enqueue a port multiple times during the same cycle!");
+      push_list_.push_back(port);
+    }
+
+    // schedule update event
+    static PoolAllocator<SimPortEvent<Pkt>, 64> s_allocator;
+    auto evt = std::allocate_shared<SimPortEvent<Pkt>>(s_allocator, port, pkt, cycles_ + delay);
     events_.emplace_back(evt);
+  }
+
+  template <typename Pkt>
+  void schedule_pop(SimPort<Pkt>* port) {
+    __assert(0 == pop_list_.count(port), "cannot dequeue a port multiple times during the same cycle!");
+    pop_list_.push_back(port);
   }
 
   std::list<SimObjectBase::Ptr> objects_;
   std::list<SimEventBase::Ptr> events_;
+  LinkedList<SimPortBase, &SimPortBase::push_list_> push_list_;
+  LinkedList<SimPortBase, &SimPortBase::pop_list_> pop_list_;
   uint64_t cycles_;
 
   template <typename U> friend class SimPort;
-  friend class SimObjectBase;
 };
 
 ///////////////////////////////////////////////////////////////////////////////
 
-inline SimObjectBase::SimObjectBase(const SimContext&, const std::string& name)
-  : name_(name)
-{}
+template <typename Pkt>
+void SimPort<Pkt>::push(const Pkt& pkt, uint64_t delay) {
+  __assert(source_ == nullptr, "cannot be called on a sink port!")
+  __assert(!this->full(), "port is full!");
+  SimPlatform::instance().schedule_push(this, pkt, delay);
+}
+
+template <typename Pkt>
+uint64_t SimPort<Pkt>::pop() {
+  __assert(sink_ == nullptr, "cannot be called on a stub port!")
+  __assert(!this->empty(), "port is empty!");
+  SimPlatform::instance().schedule_pop(this);
+  return queue_.front().cycles;
+}
+
+///////////////////////////////////////////////////////////////////////////////
 
 template <typename Impl>
 template <typename... Args>
@@ -400,11 +487,4 @@ typename SimObject<Impl>::Ptr SimObject<Impl>::Create(Args&&... args) {
   return SimPlatform::instance().create_object<Impl>(std::forward<Args>(args)...);
 }
 
-template <typename Pkt>
-void SimPort<Pkt>::push(const Pkt& pkt, uint64_t delay) const {
-  if (sink_ && !tx_cb_) {
-    reinterpret_cast<const SimPort<Pkt>*>(sink_)->push(pkt, delay);
-  } else {
-    SimPlatform::instance().schedule(this, pkt, delay);
-  }
 }
