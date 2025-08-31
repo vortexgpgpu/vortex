7410d3952e7be3d257a9a198507ecc6b935eb381
tinebp <tinebp@yahoo.com>
2025-08-31 04:07:09 -0700

    minor update

--- hw/rtl/fpu/VX_fpu_fpnew.sv
+++ hw/rtl/fpu/VX_fpu_fpnew.sv
@@ -176,7 +176,8 @@ module VX_fpu_fpnew
         fpnew_top #(
             .Features       (FPU_FEATURES),
             .Implementation (FPU_IMPLEMENTATION),
-            .TagType        (logic[(TAG_WIDTH+1)-1:0])
+            .TagType        (logic[(TAG_WIDTH+1)-1:0]),
+            .DivSqrtSel     (fpnew_pkg::PULP)
         ) fpnew_core (
             .clk_i          (clk),
             .rst_ni         (~reset),
