aa9e5669d1a3c404526f4384f2f4bcf6ca6984c1
tinebp <tinebp@yahoo.com>
2025-09-19 09:30:11 -0700

    mino update

--- tests/regression/pathfinder/common.h
+++ tests/regression/pathfinder/common.h
@@ -13,30 +13,29 @@ typedef struct {
 
   uint64_t src0_addr;
   uint64_t src1_addr;
-  uint64_t dst_addr;  
+  uint64_t dst_addr;
 } kernel_arg_t;
 
 
+#ifndef MIN
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
+#endif
+
 int *run(int *wall, int *result_s, int *src, uint32_t cols, uint32_t rows,
          uint32_t num_runs) {
-  int min;
-  int *temp;
-  int *dst;
-
+  int *dst = nullptr;
   for (uint32_t j = 0; j < num_runs; j++) {
     for (uint32_t x = 0; x < cols; x++) {
       result_s[x] = wall[x];
     }
-
     dst = result_s;
-
+    int* src_bak = src;;
     for (uint32_t t = 0; t < rows - 1; t++) {
-      temp = src;
+      int* temp = src;
       src = dst;
       dst = temp;
       for (uint32_t n = 0; n < cols; n++) {
-        min = src[n];
+        int min = src[n];
         if (n > 0)
           min = MIN(min, src[n - 1]);
         if (n < cols - 1)
@@ -45,7 +44,7 @@ int *run(int *wall, int *result_s, int *src, uint32_t cols, uint32_t rows,
       }
     }
     // Reset the pointer not to lose it
-    src = temp;
+    src = src_bak;
   }
   return dst;
 }
