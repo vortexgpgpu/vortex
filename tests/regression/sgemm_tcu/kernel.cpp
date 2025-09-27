fda2c2dbb8d32aeb9bfb45662ea498a53636743d
Giorgos Triantafyllou <giorgostriantafyllou324@gmail.com>
2025-09-27 20:16:09 +0300

    added non zero C matrix for sgemm_tcu

--- tests/regression/sgemm_tcu/kernel.cpp
+++ tests/regression/sgemm_tcu/kernel.cpp
@@ -9,6 +9,7 @@ void kernel_body(kernel_arg_t *__UNIFORM__ arg) {
   auto pA = reinterpret_cast<ctx::input_t *>(arg->A_addr);
   auto pB = reinterpret_cast<ctx::input_t *>(arg->B_addr);
   auto pC = reinterpret_cast<ctx::output_t *>(arg->C_addr);
+  auto pD = reinterpret_cast<ctx::output_t *>(arg->D_addr);
 
   uint32_t M = arg->M;
   uint32_t N = arg->N;
@@ -17,13 +18,13 @@ void kernel_body(kernel_arg_t *__UNIFORM__ arg) {
   ctx::fragment_a   fragA;
   ctx::fragment_b   fragB;
   ctx::fragment_acc fragC;
+  ctx::fragment_acc fragD;
 
   // calculate tile row & column based on block index
   uint32_t tile_row = blockIdx.y * ctx::tileM;
   uint32_t tile_col = blockIdx.x * ctx::tileN;
 
-  // Initialize accumulator tile to zero
-  ctx::fill_fragment(fragC, 0);
+  auto pTileC = pC + tile_row * N + tile_col; 
 
   for (int i = 0; i < K; i += ctx::tileK) {
     auto pTileA = pA + tile_row * K + i;
@@ -41,13 +42,16 @@ void kernel_body(kernel_arg_t *__UNIFORM__ arg) {
       ctx::load_matrix_sync(fragB, pTileB, N);
     }
 
+    // Load C tile
+    ctx::load_matrix_sync(fragC, pTileC, N);
+
     // Matrix multiply-accumulate: c += a * b
-    ctx::mma_sync(fragC, fragA, fragB, fragC);
+    ctx::mma_sync(fragD, fragA, fragB, fragC);
   }
 
   // Store the computed C tile
-  auto pTileC = pC + tile_row * N + tile_col;
-  ctx::store_matrix_sync(pTileC, fragC, N);
+  auto pTileD = pD + tile_row * N + tile_col;
+  ctx::store_matrix_sync(pTileD, fragD, N);
 }
 
 int main() {
