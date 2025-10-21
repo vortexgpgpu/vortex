7e422d718a0201135c0298985dea1c79a9e09e06
raoshilpa <shilpa.padiki.rao@gmail.com>
2025-10-21 15:06:51 -0700

    step 2 partway

---
+++ tests/regression/dot8/kernel.cpp
@@ -0,0 +1,27 @@
+#include <vx_spawn.h>
+#include "common.h"
+
+void kernel_body(kernel_arg_t* __UNIFORM__ arg) {
+	auto A = reinterpret_cast<int8_t*>(arg->A_addr);
+	auto B = reinterpret_cast<int8_t*>(arg->B_addr);
+	auto C = reinterpret_cast<int32_t*>(arg->C_addr);
+    auto size = arg->size;
+
+    int col = blockIdx.x;
+    int row = blockIdx.y;
+
+    int32_t sum(0);
+    for (int e = 0; e < size; ++e) {
+        sum += A[row * size + e] * B[e * size + col]; // int32
+    }
+
+    C[row * size + col] = sum; // 32?
+
+    // use vx_dot8?
+
+}
+
+int main() {
+	kernel_arg_t* arg = (kernel_arg_t*)csr_read(VX_CSR_MSCRATCH);
+	return vx_spawn_threads(2, arg->grid_dim, nullptr, (vx_kernel_func_cb)kernel_body, arg);
+}
