01c17fd87166eda9a68f2ffa33351017817b6874
Rahul Raj D N <rahul7rajdn@gmail.com>
2025-10-23 15:02:33 -0400

    updated time and created sbatch

--- miscs/apptainer/run_apptainer.sh
+++ miscs/apptainer/run_apptainer.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+#!/bin/bash
+#SBATCH -Jvortex-test                        # Job name
+#SBATCH -N1 --cpus-per-task=4                	 # Number of nodes and CPUs per node required
+#SBATCH --mem-per-cpu=4G                         # Memory per core
+#SBATCH -t 00:30:00                              # Duration of the job (Ex: 30 mins)
+#SBATCH -p rg-nextgen-hpc                        # Partition Name
+#SBATCH -o /tools/ci-reports/vortex-test-%j.out   # Combined output and error messages file
+#SBATCH -W                                       # Do not exit until the submitted job terminates.
+
+##Add commands here to build and execute
+## cd $GITHUB_WORKSPACE
+hostname
+#This line allows the GH runner to use the module command on the targeted node
+source /tools/misc/.read_profile
+
+
 # Set environment variables with fallback default values.
 # The :- means if the variable CONTAINER_IMAGE is not set or is empty, then use the default value vortex.sif
 export GIT_REPO_PATH=${GIT_REPO_PATH:-~/USERSCRATCH/vortex}
