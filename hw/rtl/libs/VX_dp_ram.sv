b410823736141498dda8cbfe3d7ab59398364cfa
tinebp <tinebp@yahoo.com>
2026-02-13 00:43:22 -0800

    ASIC RAM initilization fix

--- hw/rtl/libs/VX_dp_ram.sv
+++ hw/rtl/libs/VX_dp_ram.sv
@@ -13,18 +13,22 @@
 
 `include "VX_platform.vh"
 
-`define RAM_INITIALIZATION \
-    if (INIT_ENABLE != 0) begin : g_init \
-        if (INIT_FILE != "") begin : g_file \
-            initial $readmemh(INIT_FILE, ram); \
-        end else begin : g_value \
-            initial begin \
-                for (integer i = 0; i < SIZE; ++i) begin : g_i \
-                    ram[i] = INIT_VALUE; \
+`ifndef ASIC
+    `define RAM_INITIALIZATION \
+        if (INIT_ENABLE != 0) begin : g_init \
+            if (INIT_FILE != "") begin : g_file \
+                initial $readmemh(INIT_FILE, ram); \
+            end else begin : g_value \
+                initial begin \
+                    for (integer i = 0; i < SIZE; ++i) begin : g_i \
+                        ram[i] = INIT_VALUE; \
+                    end \
                 end \
             end \
-        end \
-    end
+        end
+`else
+    `define RAM_INITIALIZATION
+`endif
 
 `ifdef SIMULATION
     `define RAM_RESET_BLOCK if (RESET_RAM && reset) begin \
