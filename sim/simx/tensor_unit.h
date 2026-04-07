e0dc77a7a28bc14f7dfd3a01498f9b65a1226971
tinebp <tinebp@yahoo.com>
2026-04-06 23:34:48 -0700

    minor udpate

--- sim/simx/tensor_unit.h
+++ sim/simx/tensor_unit.h
@@ -108,7 +108,6 @@ public:
 	           uint32_t a_desc,
 	           uint32_t b_desc,
 	           const std::vector<reg_data_t>& rs1_data,
-	           const std::vector<reg_data_t>& rs2_data,
 	           const std::vector<reg_data_t>& rs3_data,
 	           std::vector<reg_data_t>& rd_data,
 	           ExeTraceData* trace_data,
